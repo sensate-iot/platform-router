3dfece700c10185e01240ea17c804fc39b8310f5
Michel Megens <dev@michelmegens.net>
2018-03-09 16:39:24 +0100

    infra: audit: implement the audit log repositories
    Implement the audit log repositories. The implementation of this
    repository differs from the 'standard' repository in that it doesn't
    allow any modification of existing records. This way nobody can update
    or remove an audit log after it has been created.
    
    Signed-off-by: Michel Megens <dev@michelmegens.net>

---
+++ SensateService/Infrastructure/Repositories/IAuditLogRepository.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * AuditLog database coupling.
+ *
+ * @author Michel Megens
+ * @email  dev@bietje.net
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using SensateService.Models;
+
+namespace SensateService.Infrastructure.Repositories
+{
+    public interface IAuditLogRepository
+    {
+		IEnumerable<AuditLog> GetByUser(SensateUser user);
+		Task<IEnumerable<AuditLog>> GetByUserAsync(SensateUser user);
+
+		IEnumerable<AuditLog> GetBetween(DateTime start, DateTime end);
+		Task<IEnumerable<AuditLog>> GetBetweenAsync(DateTime start, DateTime end);
+
+		IEnumerable<AuditLog> GetByRoute(string route);
+		Task<IEnumerable<AuditLog>> GetByRouteAsync(string route);
+
+		AuditLog Get(long id);
+		AuditLog GetById(long id);
+		Task<AuditLog> GetAsync(long id);
+
+		void Create(string route, SensateUser user = null);
+		Task CreateAsync(string route, SensateUser user = null);
+    }
+}
---
+++ SensateService/Infrastructure/Sql/AuditLogRepository.cs
@@ -0,0 +1,137 @@
+﻿/*
+ * AuditLog repository implementation.
+ * 
+ * @author Michel Megens
+ * @email  dev@bietje.net
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+
+using SensateService.Infrastructure.Repositories;
+using SensateService.Models;
+using SensateService.Exceptions;
+
+namespace SensateService.Infrastructure.Sql
+{
+	public class AuditLogRepository : AbstractSqlRepository<long, AuditLog>, IAuditLogRepository
+	{
+		public AuditLogRepository(SensateSqlContext ctx) : base(ctx)
+		{
+		}
+
+		public void Create(string route, SensateUser user = null)
+		{
+			var al = new AuditLog() {
+				Author = user,
+				Route = route,
+				Timestamp = DateTime.Now
+			};
+
+			this.Create(al);
+		}
+
+		public override void Create(AuditLog obj)
+		{
+			this.Data.Add(obj);
+			this.Commit(obj);
+		}
+
+		public async Task CreateAsync(string route, SensateUser user = null)
+		{
+			AuditLog al;
+
+			al = new AuditLog() {
+				Author = user,
+				Route = route,
+				Timestamp = DateTime.Now
+			};
+			await this.CreateAsync(al);
+		}
+
+		public override async Task CreateAsync(AuditLog obj)
+		{
+			this.Data.Add(obj);
+			await this.CommitAsync(obj);
+		}
+
+		public override void Delete(long id)
+		{
+			throw new NotAllowedException("Not allowed to delete audit log entry");
+		}
+
+		public override Task DeleteAsync(long id)
+		{
+			throw new NotAllowedException("Not allowed to delete audit log entry");
+		}
+
+		public AuditLog Get(long id)
+		{
+			AuditLog al;
+
+			al = (from log in this.Data.AsQueryable<AuditLog>()
+				  where log.Id == id
+				  select log).Single<AuditLog>();
+			return al;
+		}
+
+		public async Task<AuditLog> GetAsync(long id)
+		{
+			return await Task.Run(() => {
+				return this.Get(id);
+			});
+		}
+
+		public IEnumerable<AuditLog> GetBetween(DateTime start, DateTime end)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<IEnumerable<AuditLog>> GetBetweenAsync(DateTime start, DateTime end)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override AuditLog GetById(long id)
+		{
+			return this.Get(id);
+		}
+
+		public IEnumerable<AuditLog> GetByRoute(string route)
+		{
+			var al = from log in this.Data
+					 where log.Route == route
+					 select log;
+			return al;
+		}
+
+		public async Task<IEnumerable<AuditLog>> GetByRouteAsync(string route)
+		{
+			return await Task.Run(() => {
+				return this.GetByRoute(route);
+			});
+		}
+
+		public IEnumerable<AuditLog> GetByUser(SensateUser user)
+		{
+			var al = from log in this.Data
+					 where log.Author == user
+					 select log;
+			return al;
+		}
+
+		public async Task<IEnumerable<AuditLog>> GetByUserAsync(SensateUser user)
+		{
+			return await Task.Run(() => {
+				return this.GetByUser(user);
+			});
+		}
+
+		public override void Update(AuditLog obj)
+		{
+			throw new NotAllowedException("Not allowed to update audit log entry!");
+		}
+	}
+}
