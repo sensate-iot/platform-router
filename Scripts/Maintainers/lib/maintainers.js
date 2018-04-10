15b4e5f886840b1f66715723d1619b14b0d43bdf
Michel Megens <dev@bietje.net>
2018-04-10 12:29:37 +0200

    scripts: implement a script to find file maintainers
    Implement a script to find the file maintainer using the @author and
    @maintainer directives.
    
    Signed-off-by: Michel Megens <dev@bietje.net>

---
+++ Scripts/Maintainers/lib/maintainers.js
@@ -0,0 +1,76 @@
+/*
+ * Maintainer parsing: figure out the file maintainer of a
+ * source file.
+ * 
+ * @author Michel Megens
+ * @email  dev@bietje.net
+ */
+
+const fs = require('fs');
+
+function findName(data) {
+	var pattern = new RegExp(/@author.([^\n]+)/);
+
+	if(pattern.test(data))
+		return data.match(pattern)[1];
+
+	return null;
+}
+
+function findEmail(data) {
+	var pattern = new RegExp(/@email.([^\n]+)/);
+
+	if(pattern.test(data))
+		return data.match(pattern)[1];
+
+	return null;
+}
+
+function findMaintainer(data) {
+	var pattern = new RegExp(/@maintainer([^\n]+)/);
+
+	if(pattern.test(data))
+		return data.match(pattern)[1];
+
+	return null;
+}
+
+function searchFile(data, filename) {
+	var name = findName(data);
+	const email = findEmail(data);
+	const maintainer = findMaintainer(data);
+
+	if(maintainer != null)
+		name = maintainer;
+
+	if(name == null && maintainer == null) {
+		/* maintainer not found */
+		console.log('--------------------------');
+		console.log('No maintainer found for ' + filename);
+		console.log('--------------------------');
+		console.log('');
+
+		return;
+	}
+
+
+	console.log('--------------------------');
+	console.log('Maintainer for ' + filename + ':');
+	console.log('');
+	console.log(name + '<' + email + '>');
+	console.log('--------------------------');
+	console.log('');
+}
+
+module.exports.run = function(files) {
+	files.forEach(element => {
+		fs.readFile(element, 'utf8', (err, data) => {
+			if(err) {
+				console.log('Unable to read file: ' + err);
+				return;
+			}
+
+			searchFile(data, element);
+		});
+	});
+}
