f37d9956286b6ec6d0d1bdbe34e41758bac22ddd
Michel Megens <dev@bietje.net>
2018-04-16 16:00:21 +0200

    infra: stats: add the sensor statistics models
    Implement the sensor statistics model.
    
    Signed-off-by: Michel Megens <dev@bietje.net>

---
+++ Core/Infrastructure/Repositories/ISensorStatistics.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Sensor statistics DI interface.
+ *
+ * @author Michel Megens
+ * @email  dev@bietje.net
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using SensateService.Models;
+
+namespace SensateService.Infrastructure.Repositories
+{
+	public interface ISensorStatistics
+	{
+		Task IncrementAsync(Sensor sensor);
+		Task<SensorStatisticsEntry> CreateForAsync(Sensor sensor);
+
+		Task<SensorStatisticsEntry> GetByDateAsync(Sensor sensor, DateTime date);
+		Task<IEnumerable<SensorStatisticsEntry>> GetBeforeAsync(Sensor sensor, DateTime date);
+		Task<IEnumerable<SensorStatisticsEntry>> GetAfterAsync(Sensor sensor, DateTime date);
+
+		void Delete(string id);
+		Task DeleteAsync(string id);
+		Task DeleteBySensorAsync(Sensor sensor);
+	}
+}
---
+++ Core/Models/SensorStatisticsEntry.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Statistics entry for a single day for a specific sensor.
+ *
+ * @author Michel Megens
+ * @email  dev@bietje.net
+ */
+
+using System;
+
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using Newtonsoft.Json;
+
+using SensateService.Converters;
+
+namespace SensateService.Models
+{
+	public class SensorStatisticsEntry
+	{
+		[BsonId, BsonRequired, JsonConverter(typeof(ObjectIdJsonConverter))]
+		public ObjectId InternalId { get; set; }
+		[BsonRequired, JsonConverter(typeof(ObjectIdJsonConverter))]
+		public ObjectId SensorId { get; set; }
+		[BsonRequired]
+		public DateTime Date { get; set; }
+		[BsonRequired]
+		public int Measurements { get; set; }
+	}
+}
