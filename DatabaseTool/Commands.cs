efd602947448240c9cd5517df0f362bc94aeba5c
Michel Megens <dev@bietje.net>
2018-09-26 15:33:31 +0200

    dbtool: implement a database tool to query data sources
    Implement a tool to query and test data sources and repository
    implementations.
    
    Signed-off-by: Michel Megens <dev@bietje.net>

---
+++ DatabaseTool/Commands.cs
@@ -0,0 +1,132 @@
+﻿/*
+ * Collection of command implementations.
+ *
+ * @author Michel Megens
+ * @email  dev@bietje.net
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using SensateService.Helpers;
+using SensateService.Infrastructure.Repositories;
+using SensateService.Models;
+
+namespace SensateService.DatabaseTool
+{
+	public delegate Task CliCommand(IServiceScope scope, string argument);
+
+	public class Commands
+	{
+		public static async Task GetMeasurementsBySensor(IServiceScope scope, string args)
+		{
+			Stopwatch sw;
+
+			if(args.Length <= 0) {
+				Console.WriteLine("> usage: GetMeasurementsBySensor <sensorID>");
+				return;
+			}
+
+			var repo = scope.ServiceProvider.GetRequiredService<IMeasurementRepository>();
+			var sensors = scope.ServiceProvider.GetRequiredService<ISensorRepository>();
+
+			try {
+				var sensor = await sensors.GetAsync(args).AwaitSafely();
+				sw = new Stopwatch();
+
+				if(sensor == null)
+					Console.WriteLine("> Unknown sensor ID");
+
+				sw.Start();
+				var measurements = await repo.GetMeasurementsBySensorAsync(sensor).AwaitSafely();
+				sw.Stop();
+
+				var enumerable = measurements as List<Measurement> ?? measurements.ToList();
+				var json = JsonConvert.SerializeObject(enumerable.TakeLast(100), Formatting.Indented);
+
+				Console.WriteLine(json);
+				Console.WriteLine($"> Number of measurements stored: {enumerable.Count}");
+				Console.WriteLine($"> Needed {sw.Elapsed} to complete query!");
+			} catch(IndexOutOfRangeException) {
+				Console.WriteLine("> Unknown sensor ID");
+			}
+		}
+
+		public static async Task GetMeasurementsAfterYesterday(IServiceScope scope, string args)
+		{
+			Stopwatch sw;
+
+			if(args.Length <= 0) {
+				Console.WriteLine("> usage: GetMeasurementsBySensor <sensorID>");
+				return;
+			}
+
+			var repo = scope.ServiceProvider.GetRequiredService<IMeasurementRepository>();
+			var sensors = scope.ServiceProvider.GetRequiredService<ISensorRepository>();
+
+			try {
+				var sensor = await sensors.GetAsync(args).AwaitSafely();
+				sw = new Stopwatch();
+
+				if(sensor == null)
+					Console.WriteLine("> Unknown sensor ID");
+
+				var today = DateTime.Today;
+				var yday = today.AddDays(-1D);
+
+				sw.Start();
+				var measurements = await repo.GetAfterAsync(sensor, yday).AwaitSafely();
+				sw.Stop();
+
+				var enumerable = measurements as List<Measurement> ?? measurements.ToList();
+				var json = JsonConvert.SerializeObject(enumerable.TakeLast(100), Formatting.Indented);
+
+				Console.WriteLine(json);
+				Console.WriteLine($"> Number of measurements stored: {enumerable.Count}");
+				Console.WriteLine($"> Needed {sw.Elapsed} to complete query!");
+			} catch(IndexOutOfRangeException) {
+				Console.WriteLine("> Unknown sensor ID");
+			}
+		}
+
+		public static async Task GetMeasurementsBeforeToday(IServiceScope scope, string args)
+		{
+			Stopwatch sw;
+
+			if(args.Length <= 0) {
+				Console.WriteLine("> usage: GetMeasurementsBySensor <sensorID>");
+				return;
+			}
+
+			var repo = scope.ServiceProvider.GetRequiredService<IMeasurementRepository>();
+			var sensors = scope.ServiceProvider.GetRequiredService<ISensorRepository>();
+
+			try {
+				var sensor = await sensors.GetAsync(args).AwaitSafely();
+				sw = new Stopwatch();
+
+				if(sensor == null)
+					Console.WriteLine("> Unknown sensor ID");
+
+				var today = DateTime.Today;
+
+				sw.Start();
+				var measurements = await repo.GetBeforeAsync(sensor, today).AwaitSafely();
+				sw.Stop();
+
+				var enumerable = measurements as List<Measurement> ?? measurements.ToList();
+				var json = JsonConvert.SerializeObject(enumerable.TakeLast(100), Formatting.Indented);
+
+				Console.WriteLine(json);
+				Console.WriteLine($"> Number of measurements stored: {enumerable.Count}");
+				Console.WriteLine($"> Needed {sw.Elapsed} to complete query!");
+			} catch(IndexOutOfRangeException) {
+				Console.WriteLine("> Unknown sensor ID");
+			}
+		}
+	}
+}
---
+++ DatabaseTool/Startup.cs
@@ -0,0 +1,111 @@
+﻿/*
+ * DatabaseTool startup.
+ *
+ * @author Michel Megens
+ * @email dev@bietje.net
+ */
+
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+using SensateService.Config;
+using SensateService.Infrastructure.Sql;
+using SensateService.Init;
+using SensateService.Models;
+
+namespace SensateService.DatabaseTool
+{
+	public class Startup
+	{
+		private readonly IConfiguration Configuration;
+		private readonly ManualResetEvent _reset;
+
+		public Startup()
+		{
+			var builder = new ConfigurationBuilder();
+
+			this._reset = new ManualResetEvent(false);
+			Console.CancelKeyPress += this.CancelEvent_Handler;
+
+			builder.SetBasePath(Path.Combine(AppContext.BaseDirectory));
+			builder.AddEnvironmentVariables();
+
+			if(IsDevelopment())
+				builder.AddUserSecrets<Startup>();
+			else
+                builder.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+
+			Configuration = builder.Build();
+		}
+
+		private static bool IsDevelopment()
+		{
+			var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development";
+			return env == "Development";
+		}
+
+		public void Reset()
+		{
+			this._reset.Set();
+		}
+
+		private void CancelEvent_Handler(object sender, ConsoleCancelEventArgs e)
+		{
+			this._reset.Set();
+			e.Cancel = true;
+		}
+
+		public void ConfigureServices(IServiceCollection services)
+		{
+			var db = new DatabaseConfig();
+			var cache = new CacheConfig();
+
+			Configuration.GetSection("Database").Bind(db);
+			Configuration.GetSection("Cache").Bind(cache);
+
+			services.AddPostgres(db.PgSQL.ConnectionString);
+
+			services.AddIdentity<SensateUser, UserRole>(config => {
+				config.SignIn.RequireConfirmedEmail = true;
+			})
+			.AddEntityFrameworkStores<SensateSqlContext>()
+			.AddDefaultTokenProviders();
+
+			services.AddLogging(builder => { builder.AddConfiguration(this.Configuration.GetSection("Logging")); });
+
+
+			if(cache.Enabled)
+                services.AddCacheStrategy(cache, db);
+
+			services.AddDocumentStore(db.MongoDB.ConnectionString, db.MongoDB.DatabaseName);
+			services.AddDocumentRepositories(cache.Enabled);
+			services.AddSqlRepositories();
+		}
+
+		public void Configure(IServiceProvider provider)
+		{
+			var logging = provider.GetRequiredService<ILoggerFactory>();
+
+			logging.AddConsole();
+
+			if(IsDevelopment())
+                logging.AddDebug();
+		}
+
+		public async Task Run(IServiceProvider provider)
+		{
+			CliParser parser = new CliParser();
+
+			var tsk = parser.Run(provider);
+			this._reset.WaitOne();
+			await tsk;
+		}
+	}
+}
