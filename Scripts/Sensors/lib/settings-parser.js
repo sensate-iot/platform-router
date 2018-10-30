8a4e40286a166131abfc1c49c1d037941280b842
Michel Megens <dev@bietje.net>
2018-10-30 13:27:40 +0100

    scripts: sensors: add config file parser
    Implement a config file parser to the sensor mocking tool. Doing so
    'secrets' can be stored in a config file. Supported configuration tags
    can be viewed in the example configuration file.
    
    Signed-off-by: Michel Megens <dev@bietje.net>

---
+++ Scripts/Sensors/lib/settings-parser.js
@@ -0,0 +1,20 @@
+/*
+ * User settings parser.
+ * 
+ * @author Michel Megens
+ * @email  dev@bietje.net
+ */
+
+const fs = require('fs');
+
+function parse(path) {
+	if(path == undefined)
+		return undefined;
+
+	console.log(path)
+	const json = fs.readFileSync(path);
+
+	return JSON.parse(json);
+}
+
+module.exports = {parse}
