a1837d126ca9540f4143f71eec26f125ec6c3115
Michel Megens <dev@bietje.net>
2019-02-27 16:58:01 +0100

    services: processing: add timed background service
    Implement a background service based on the System.Threading.Timer
    timer. Said background process is invoked at a predetermined interval.
    The interval is configured by overriding the `Configure()' method and
    the `TimedBackgroundServiceSettings' class.
    
    Signed-off-by: Michel Megens <dev@bietje.net>

---
+++ Core/Services/Processing/TimedBackgroundService.cs
@@ -0,0 +1,46 @@
+﻿/*
+ * Background service based on System.Threading.Timer.
+ *
+ * @author Michel Megens
+ * @email  dev@bietje.net
+ */
+
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Hosting;
+
+using SensateService.Services.Settings;
+
+namespace SensateService.Services.Processing
+{
+	public abstract class TimedBackgroundService : IHostedService
+	{
+		private Timer _timer;
+
+		public Task StartAsync(CancellationToken cancellationToken)
+		{
+			var settings = new TimedBackgroundServiceSettings();
+
+			this.Configure(settings);
+			this._timer = new Timer(this.Invoke, null, settings.StartDelay, settings.Interval);
+
+			return Task.CompletedTask;
+		}
+
+		public Task StopAsync(CancellationToken cancellationToken)
+		{
+			this._timer.Change(Timeout.Infinite, Timeout.Infinite);
+			return Task.CompletedTask;
+		}
+
+		public void Invoke(object arg)
+		{
+			var task = Task.Run(async () => { await this.ProcessAsync(); });
+			task.Wait();
+		}
+
+		protected abstract Task ProcessAsync();
+		protected abstract void Configure(TimedBackgroundServiceSettings settings);
+	}
+}
---
+++ Core/Services/Settings/TimedBackgroundServiceSettings.cs
@@ -0,0 +1,15 @@
+﻿/*
+ * Settings for TimedBackgroundService.
+ *
+ * @author Michel Megens
+ * @email  dev@bietje.net
+ */
+
+namespace SensateService.Services.Settings
+{
+	public class TimedBackgroundServiceSettings
+	{
+		public int Interval { get; set; }
+		public int StartDelay { get; set; }
+	}
+}
