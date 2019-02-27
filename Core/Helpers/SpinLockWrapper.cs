951430f56082c33d3ef223b83c107a93fb405ec6
Michel Megens <dev@bietje.net>
2019-02-27 16:36:31 +0100

    core: helpers: implement spin lock wrapper
    Implement a wrapper class around spin locks. Said wrapper ensures that a
    lock is obtained through the `Lock()' method. If the calling thread
    fails to obtain the lock an exception is thrown.
    
    Signed-off-by: Michel Megens <dev@bietje.net>

---
+++ Core/Helpers/SpinLockWrapper.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * Wrapper class for SpinLocks.
+ *
+ * @author Michel Megens
+ * @email  dev@bietje.net
+ */
+
+using System.Threading;
+
+namespace SensateService.Helpers
+{
+	public struct SpinLockWrapper
+	{
+		private SpinLock _lock;
+
+		public bool IsLocked { get; private set; }
+
+		public SpinLockWrapper(bool tracking = false)
+		{
+			this._lock = new SpinLock(tracking);
+			this.IsLocked = false;
+		}
+
+		public void Lock()
+		{
+			var locked = false;
+
+			this._lock.Enter(ref locked);
+			this.IsLocked = locked;
+
+			if(!locked)
+				throw new SynchronizationLockException("Unable to obtain spin lock!");
+		}
+
+		public void Unlock()
+		{
+			if(!this.IsLocked)
+				return;
+
+			this.IsLocked = false;
+			this._lock.Exit();
+		}
+	}
+}
