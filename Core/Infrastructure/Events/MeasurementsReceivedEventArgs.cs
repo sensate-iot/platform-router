80a0cbe9568619a766993ed6e133ea55939437cd
Michel Megens <dev@bietje.net>
2019-03-01 10:35:58 +0100

    infra: events: implement bulk measurement event
    Implement an event that can be used when the measurement insert cache
    processes an entire collection of previously received measurements.
    
    Signed-off-by: Michel Megens <dev@bietje.net>

---
+++ Core/Infrastructure/Events/MeasurementsReceivedEventArgs.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Bulk measurement processing event.
+ *
+ * @author Michel Megens
+ * @email  dev@bietje.net
+ */
+
+using System.Collections.Generic;
+using System.Threading;
+using SensateService.Models;
+
+namespace SensateService.Infrastructure.Events
+{
+	public class MeasurementsReceivedEventArgs
+	{
+		public IList<Measurement> Measurements { get; }
+		public CancellationToken Token { get; }
+
+		public MeasurementsReceivedEventArgs(IList<Measurement> measurements, CancellationToken token)
+		{
+			this.Measurements = measurements;
+			this.Token = token;
+		}
+	}
+}
