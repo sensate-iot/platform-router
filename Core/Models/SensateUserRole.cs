8cb3ada22ab6ada7d57b56bc63c704002ebb1736
Michel Megens <dev@bietje.net>
2019-03-11 15:11:29 +0100

    models: roles: add `JsonIgnore' attributes to user roles
    Add the `JsonIgnore' attribute to the `SensateUserRole' model class.
    
    Signed-off-by: Michel Megens <dev@bietje.net>

--- Core/Models/SensateUserRole.cs
+++ Core/Models/SensateUserRole.cs
@@ -6,12 +6,15 @@
  */
 
 using Microsoft.AspNetCore.Identity;
+using Newtonsoft.Json;
 
 namespace SensateService.Models
 {
 	public class SensateUserRole : IdentityUserRole<string>
 	{
+		[JsonIgnore]
 		public virtual SensateUser User { get; set; }
+		[JsonIgnore]
 		public virtual SensateRole Role { get; set; }
 	}
 }
