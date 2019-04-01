de63fe91077807e94f291e2f4fbdf0c277272c05
Michel Megens <dev@bietje.net>
2019-04-01 20:40:05 +0200

    data-api: sensors: add the sensors controller
    Add a sensors controller with an index endpoint.
    
    Signed-off-by: Michel Megens <dev@bietje.net>

---
+++ DataApi/Controllers/SensorsController.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * Sensor HTTP controller.
+ *
+ * @author Michel Megens
+ * @email  dev@bietje.net
+ */
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using SensateService.ApiCore.Controllers;
+using SensateService.Helpers;
+using SensateService.Infrastructure.Repositories;
+using SensateService.Models;
+
+namespace SensateService.DataApi.Controllers
+{
+	[Produces("application/json")]
+	[Route("[controller]")]
+	public class SensorsController : AbstractApiController 
+	{
+		private readonly ISensorRepository _sensors;
+
+		public SensorsController(IHttpContextAccessor ctx, ISensorRepository sensors) : base(ctx)
+		{
+			this._sensors = sensors;
+		}
+
+		[HttpGet]
+		[ProducesResponseType(typeof(IEnumerable<Sensor>), 200)]
+		public async Task<IActionResult> Index([FromQuery] string name)
+		{
+			IEnumerable<Sensor> sensors;
+
+			if(string.IsNullOrEmpty(name))
+				sensors = await this._sensors.GetAsync(this.CurrentUser).AwaitBackground();
+			else
+				sensors = await this._sensors.FindByNameAsync(this.CurrentUser, name).AwaitBackground();
+				
+			return this.Ok(sensors);
+		}
+	}
+}
