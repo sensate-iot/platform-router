149e7bf415a4f674a5761d098b3de876aa9bcd35
Michel Megens <michel@michelmegens.net>
2020-01-25 21:23:01 +0100

    api: data: implement individual measurement getter
    Implement a getter based on a measurement index.
    
    Signed-off-by: Michel Megens <michel@michelmegens.net>

--- DataApi/Controllers/MeasurementsController.cs
+++ DataApi/Controllers/MeasurementsController.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-
+using MongoDB.Bson;
 using SensateService.ApiCore.Attributes;
 using SensateService.ApiCore.Controllers;
 using SensateService.Enums;
@@ -18,6 +18,7 @@ using SensateService.Helpers;
 using SensateService.Infrastructure.Repositories;
 using SensateService.Infrastructure.Storage;
 using SensateService.Models;
+using SensateService.Models.Generic;
 using SensateService.Models.Json.In;
 using SensateService.Models.Json.Out;
 
@@ -25,17 +26,15 @@ namespace SensateService.DataApi.Controllers
 {
 	[Produces("application/json")]
 	[Route("[controller]")]
-	public class MeasurementsController : AbstractApiController 
+	public class MeasurementsController : AbstractDataController 
 	{
-		private readonly IMeasurementCache _store;
-		private readonly IMeasurementRepository _repo;
-		private readonly ISensorRepository _sensors;
+		private readonly IMeasurementCache m_cache;
+		private readonly IMeasurementRepository m_measurements;
 
-		public MeasurementsController(IMeasurementCache cache, IMeasurementRepository repo, ISensorRepository sensors, IHttpContextAccessor ctx) : base(ctx)
+		public MeasurementsController(IMeasurementCache cache, IMeasurementRepository measurements, ISensorRepository sensors, IHttpContextAccessor ctx) : base(ctx, sensors)
 		{
-			this._store = cache;
-			this._repo = repo;
-			this._sensors = sensors;
+			this.m_cache = cache;
+			this.m_measurements = measurements;
 		}
 
 		[HttpPost("create")]
@@ -59,21 +58,47 @@ namespace SensateService.DataApi.Controllers
 			status.ErrorCode = ReplyCode.Ok;
 			status.Message = "Measurement queued!";
 
-			await this._store.StoreAsync(raw, RequestMethod.HttpPost).AwaitBackground();
+			await this.m_cache.StoreAsync(raw, RequestMethod.HttpPost).AwaitBackground();
 			return this.Ok(status);
+		}
+
+		[HttpGet("{bucketId}/{index}")]
+		[ProducesResponseType(200)]
+		public async Task<IActionResult> Get( string bucketId, int index = -1)
+		{
+			if(bucketId == null && index < 0) {
+				return this.UnprocessableEntity(new Status {
+					Message = "Invalid query",
+					ErrorCode = ReplyCode.BadInput
+				});
+			}
+
+			var idx = new MeasurementIndex { Index = index };
+
+			if(!ObjectId.TryParse(bucketId, out var id)) {
+				return this.UnprocessableEntity(new Status {
+					Message = "Invalid sensor ID",
+					ErrorCode = ReplyCode.BadInput
+				});
+			}
+
+			idx.MeasurementBucketId = id;
+
+			var measurement = await this.m_measurements.GetMeasurementAsync(idx).AwaitBackground();
+			return this.Ok(measurement);
 		}
 
 		[HttpGet]
 		[ProducesResponseType(200)]
 		public async Task<IActionResult> Get([FromQuery] string sensorId, [FromQuery] DateTime start, [FromQuery] DateTime end, [FromQuery] int skip = -1, [FromQuery] int limit = -1)
 		{
-			var sensor = await this._sensors.GetAsync(sensorId).AwaitBackground();
+			var sensor = await this.m_sensors.GetAsync(sensorId).AwaitBackground();
 
 			// TODO: check for admin
 			if(sensor.Owner != this.CurrentUser.Id)
 				return this.Unauthorized();
 
-			var data = await this._repo.GetBetweenAsync(sensor, start, end, skip, limit).AwaitBackground();
+			var data = await this.m_measurements.GetBetweenAsync(sensor, start, end, skip, limit).AwaitBackground();
 			return this.Ok(data);
 		}
 	}
