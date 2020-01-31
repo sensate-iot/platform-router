0be31cde8f20972d3ea3142630330cae254c84b2
Michel Megens <michel@michelmegens.net>
2020-01-31 14:22:37 +0100

    core: models: add an actuator control message model
    Add a model for messages which are (read: can be) sent to actuator
    devices.
    
    Signed-off-by: Michel Megens <michel@michelmegens.net>

---
+++ Core/Models/ControlMessage.cs
@@ -0,0 +1,17 @@
+﻿/*
+ * Control message model for actuators.
+ *
+ * @author Michel Megens
+ * @email  michel@michelmegens.net
+ */
+
+namespace SensateService.Models
+{
+	public class ControlMessage
+	{
+        public long Id { get; set; }
+        public int NodeId { get; set; }
+        public string SensorId { get; set; }
+        public string Data { get; set; }
+	}
+}
