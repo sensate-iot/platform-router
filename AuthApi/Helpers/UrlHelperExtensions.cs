f117aa684d168a685696d2abbc87197e0449c063
Michel Megens <michel@michelmegens.net>
2020-02-14 14:11:25 +0100

    auth-api: remove unused definitions
    Remove unused const statements.
    
    Signed-off-by: Michel Megens <michel@michelmegens.net>

--- AuthApi/Helpers/UrlHelperExtensions.cs
+++ AuthApi/Helpers/UrlHelperExtensions.cs
@@ -5,18 +5,12 @@
  * @email  michel.megens@sonatolabs.com
  */
 
-using System;
-using System.Diagnostics.CodeAnalysis;
 using Microsoft.AspNetCore.Mvc;
-using SensateService.AuthApi.Controllers;
 
 namespace SensateService.AuthApi.Helpers
 {
     public static class UrlHelperExtensions
     {
-		private static string TokenIdentifier = "{token}";
-
-		[SuppressMessage("ReSharper", "RedundantAnonymousTypePropertyName")]
 		public static string EmailConfirmationLink(this IUrlHelper url, string id, string code, string scheme, string host)
 		{
 			return $"{scheme}://{host}/{id}/{code}";
