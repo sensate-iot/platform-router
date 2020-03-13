a5e67ef1286b8e86de28dcccce1eadba683bbaa9
Michel Megens <michel@michelmegens.net>
2020-03-13 16:35:27 +0100

    core: helpers: implement a `DistinctBy' extension
    Extend the functionality of `IEnumerable' with a `DistinctBy' method, in
    order to find the distinct based on a given property.
    
    Signed-off-by: Michel Megens <michel@michelmegens.net>

---
+++ Core/Helpers/IEnumerableHelper.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Helper methods for IEnumerable.
+ *
+ * @author Michel Megens
+ * @email  michel@michelmegens.net
+ */
+
+using System;
+using System.Collections.Generic;
+
+namespace SensateService.Helpers
+{
+	public static class IEnumerableHelper
+	{
+		public static IEnumerable<T> DistinctBy<T, V>(this IEnumerable<T> source, Func<T, V> selector)
+		{
+			var seen = new HashSet<V>();
+
+			foreach(var element in source) {
+				if(seen.Add(selector(element))) {
+					yield return element;
+				}
+			}
+		}
+	}
+}
