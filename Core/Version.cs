bd753b6237cff912cb12f64e48e5b69fc1cf92c3
Michel Megens <michel@michelmegens.net>
2020-03-18 11:19:55 +0100

    Sensate IoT 0.4.2
    Signed-off-by: Michel Megens <michel@michelmegens.net>

--- Core/Version.cs
+++ Core/Version.cs
@@ -5,19 +5,14 @@
  * @email  michel.megens@sonatolabs.com
  */
 
-using System;
-
 namespace SensateService
 {
 	public static class Version
 	{
-		public static string VersionString => String.Format(
-			"Sensate Core {0}.{1}.{2}",
-			Major, Minor, PatchLevel
-		);
+		public static string VersionString => $"Sensate Core {Major}.{Minor}.{PatchLevel}";
 
 		public const int Major = 0;
 		public const int Minor = 4;
-		public const int PatchLevel = 1;
+		public const int PatchLevel = 2;
 	}
 }
