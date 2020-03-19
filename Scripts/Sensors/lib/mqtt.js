69f03e30fbc5157931927269341d3cc003746c35
Michel Megens <michel@michelmegens.net>
2020-03-19 18:05:40 +0100

    scripts: sensors: improve websocket handling
    Improve the handling of websockets.
    
    Signed-off-by: Michel Megens <michel@michelmegens.net>

--- Scripts/Sensors/lib/mqtt.js
+++ Scripts/Sensors/lib/mqtt.js
@@ -9,71 +9,10 @@
 
 var mqtt = require('mqtt');
 var NanoTimer = require('nanotimer');
-var crypto = require('crypto');
-
-function getRandNumber(min, max) {
-	return Math.floor(Math.random() * ( max - min)) + min;
-}
-
-function generateLocationAround(lat, lng, radius) {
-	const x0 = lng;
-	const y0 = lat;
-	const deg = radius / 111300;
-
-	const m = Math.random();
-	const n = Math.random();
-
-	const w = deg * Math.sqrt(m);
-	const t = 2 * Math.PI * n;
-	const x = w * Math.cos(t);
-	const y = w * Math.sin(t);
-
-	const xp = x / Math.cos(y0);
-
-	return [
-		y + y0,
-		xp + x0
-	]
-}
-
-function generateMeasurement(args) {
-	let idx = 0;
-
-	if(args.allsensors) {
-		idx = getRandNumber(0, args.sensors.length);
-	}
-
-	const location = generateLocationAround(51.59137, 4.7786, 1200);
-
-	const measurement = {
-		Longitude: location[1],
-		Latitude: location[0],
-		CreatedById: args.sensors[idx].sensor,
-		CreatedBySecret: args.sensors[idx].secret,
-		Data: {
-			x: {
-				Value: Math.random() * 10,
-				Unit: "m/s2"
-			},
-			y: {
-				Value: Math.random() * 100,
-				Unit: "m/s2"
-			},
-			z: {
-				Value: Math.random() * 20,
-				Unit: "m/s2"
-			}
-		}
-	}
-
-	const hash = crypto.createHash('sha256').update(JSON.stringify(measurement));
-	measurement.CreatedBySecret = `$${hash.digest('hex')}==`;
-
-	return measurement;
-}
+const generate = require('./generate');
 
 function publish(client, args) {
-	const measurement = generateMeasurement(args, args);
+	const measurement = generate.generateMeasurement(args, args);
 	client.publish('sensate/measurements', JSON.stringify(measurement));
 }
 
--- Scripts/Sensors/lib/websocket.js
+++ Scripts/Sensors/lib/websocket.js
@@ -6,28 +6,10 @@
  */
 
 const socket = require('ws');
+const generate = require('./generate');
 
 function publish(ws, args) {
-	const measurement = {
-		Longitude: 5.13613511,
-		Latitude: 55.215135211,
-		CreatedById: args.id,
-		CreatedBySecret: args.secret,
-		Data: {
-			x: {
-				Value: Math.random() * 10,
-				Unit: "m/s2"
-			},
-			y: {
-				Value: Math.random() * 100,
-				Unit: "m/s2"
-			},
-			z: {
-				Value: Math.random() * 20,
-				Unit: "m/s2"
-			}
-		}
-	}
+	const measurement = generate.generateMeasurement(args);
 
 	ws.send(JSON.stringify(measurement));
 }
