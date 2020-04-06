4fcbf064700ac63fcd5db3180508ded13db04def
Michel Megens <michel@michelmegens.net>
2020-04-06 16:13:46 +0200

    api-core: correctly implement API key filters
    The functionality of the admin key filter and the read-write API-key
    filter has accidentaly been swapped.
    
    Signed-off-by: Michel Megens <michel@michelmegens.net>

--- ApiCore/Attributes/ReadWriteApiKeyAttribute.cs
+++ ApiCore/Attributes/ReadWriteApiKeyAttribute.cs
@@ -5,44 +5,30 @@
  * @email  michel.megens@sonatolabs.com
  */
 
-using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
-using SensateService.Enums;
+
 using SensateService.Models;
-using SensateService.Models.Json.Out;
-using UserRoles = SensateService.Constants.UserRoles;
 
 namespace SensateService.ApiCore.Attributes
 {
 	public class ReadWriteApiKeyAttribute : ActionFilterAttribute
 	{
-		private static bool IsInRole(SensateUser user, string role)
-		{
-			var roles = user.UserRoles.Select(x => x.Role.NormalizedName);
-			return roles.Contains(role);
-		}
-
 		public override void OnActionExecuting(ActionExecutingContext context)
 		{
-			Status status;
 			var valid = false;
 
 			base.OnActionExecuting(context);
 
 			if(context.HttpContext.Items["ApiKey"] is SensateApiKey key && !key.Revoked) {
-				valid = !key.ReadOnly && IsInRole(key.User, UserRoles.Administrator);
+				valid = !key.ReadOnly;
 			}
 
-			if(valid)
+			if(valid) {
 				return;
+			}
 
-			status = new Status {
-				ErrorCode = ReplyCode.NotAllowed,
-				Message = "Invalid API key"
-			};
-
-			context.Result = new BadRequestObjectResult(status);
+			context.Result = new ForbidResult();
 		}
 	}
 }
