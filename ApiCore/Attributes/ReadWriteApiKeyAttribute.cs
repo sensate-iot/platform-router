9d5f1419d43e3fc349dbd90bbaed3c07dd9e0713
Michel Megens <michel@michelmegens.net>
2020-04-06 10:54:51 +0200

    apicode: categorize using statements
    Categorize using statements by adding newlines.
    
    Signed-off-by: Michel Megens <michel@michelmegens.net>

--- ApiCore/Attributes/ReadWriteApiKeyAttribute.cs
+++ ApiCore/Attributes/ReadWriteApiKeyAttribute.cs
@@ -5,16 +5,24 @@
  * @email  michel.megens@sonatolabs.com
  */
 
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using SensateService.Enums;
 using SensateService.Models;
 using SensateService.Models.Json.Out;
+using UserRoles = SensateService.Constants.UserRoles;
 
 namespace SensateService.ApiCore.Attributes
 {
 	public class ReadWriteApiKeyAttribute : ActionFilterAttribute
 	{
+		private static bool IsInRole(SensateUser user, string role)
+		{
+			var roles = user.UserRoles.Select(x => x.Role.NormalizedName);
+			return roles.Contains(role);
+		}
+
 		public override void OnActionExecuting(ActionExecutingContext context)
 		{
 			Status status;
@@ -23,7 +31,7 @@ namespace SensateService.ApiCore.Attributes
 			base.OnActionExecuting(context);
 
 			if(context.HttpContext.Items["ApiKey"] is SensateApiKey key && !key.Revoked) {
-				valid = !key.ReadOnly;
+				valid = !key.ReadOnly && IsInRole(key.User, UserRoles.Administrator);
 			}
 
 			if(valid)
