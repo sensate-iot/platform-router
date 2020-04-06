ef8688f129f4f5534d4f3c20a0502782252c34c1
Michel Megens <michel@michelmegens.net>
2020-04-06 10:55:35 +0200

    api-core: attributes: add attribute for admin keys
    Add an attribute to check for administrative admin keys.
    
    Signed-off-by: Michel Megens <michel@michelmegens.net>

---
+++ ApiCore/Attributes/AdminApiKeyAttribute.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * Attribute to verify user status.
+ *
+ * @author Michel Megens
+ * @email  michel@michelmegens.net
+ */
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+using SensateService.Models;
+
+namespace SensateService.ApiCore.Attributes
+{
+	public class AdminApiKeyAttribute : ActionFilterAttribute
+	{
+		public override void OnActionExecuting(ActionExecutingContext context)
+		{
+			var valid = false;
+
+			base.OnActionExecuting(context);
+
+			if(context.HttpContext.Items["ApiKey"] is SensateApiKey key && !key.Revoked) {
+				valid = !key.ReadOnly;
+			}
+
+			if(valid) {
+				return;
+			}
+
+			context.Result = new ForbidResult();
+		}
+	}
+}
