893a8e56cf31414f4534cddea9bf13fa1b811e88
Michel Megens <michel@michelmegens.net>
2020-04-09 19:02:43 +0200

    core: models: add billing lockout property
    Add a property to users to indicate a pending payment action.
    
    Signed-off-by: Michel Megens <michel@michelmegens.net>

--- Core/Models/SensateUser.cs
+++ Core/Models/SensateUser.cs
@@ -9,6 +9,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+
 using Microsoft.AspNetCore.Identity;
 
 namespace SensateService.Models
@@ -23,6 +24,8 @@ namespace SensateService.Models
 		public string UnconfirmedPhoneNumber { get; set; }
 		[Required]
 		public DateTime RegisteredAt { get; set; }
+		[Required]
+		public bool BillingLockout { get; set; }
 		public virtual ICollection<SensateUserRole> UserRoles { get; set; }
 		public virtual ICollection<SensateApiKey> ApiKeys { get; set; }
 	}
