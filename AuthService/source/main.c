4b482226d51966c5b89403652580e5f5ce36f640
Michel Megens <michel@michelmegens.net>
2020-04-16 10:29:34 +0200

    auth-service: add authentication service project
    Add an authentication service project. This project is developed in
    order to improve the data ingress performance.
    
    Signed-off-by: Michel Megens <michel@michelmegens.net>

---
+++ AuthService/source/main.c
@@ -0,0 +1,15 @@
+/*
+ * Program entry point.
+ *
+ * @author Michel Megens
+ * @email  michel@michelmegens.net
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+	printf("Started!\n");
+	return -EXIT_SUCCESS;
+}
