bab187fb1639aeceb852191da9b51367222759e3
Michel Megens <michel@michelmegens.net>
2020-04-26 17:44:03 +0200

    project: tests: add unit and integration tests
    Signed-off-by: Michel Megens <michel@michelmegens.net>

---
+++ AuthService/tests/messageservice_test.cpp
@@ -0,0 +1,56 @@
+/*
+ * Message service integration test.
+ *
+ * @author Michel Megens
+ * @email  michel@michelmegens.net
+ */
+
+#include <cstdlib>
+#include <thread>
+#include <chrono>
+
+#include <sensateiot/imqttclient.h>
+#include <sensateiot/mqttclient.h>
+#include <sensateiot/messageservice.h>
+
+#include "testmqttclient.h"
+
+int main(int argc, char** argv)
+{
+	sensateiot::config::Config config;
+
+	config.SetWorkers(3);
+	config.SetInterval(1000);
+
+	auto& mqtt = config.GetMqtt().GetPrivateBroker();
+	mqtt.GetBroker().SetHostName("tcp://127.0.0.1");
+	mqtt.GetBroker().SetPort(1883);
+
+	sensateiot::test::TestMqttClient client;
+	client.Connect(config.GetMqtt());
+
+	sensateiot::mqtt::MessageService service(client, config);
+
+	using namespace std::chrono_literals;
+	auto t1 = std::thread([&]() {
+		service.AddMessage("t1 Hello 1\n");
+		service.AddMessage("t1 Hello 2\n");
+		std::this_thread::sleep_for(1ms);
+		service.AddMessage("t1 Hello 3\n");
+		service.AddMessage("t1 Hello 4\n");
+		service.Process();
+	});
+
+	auto t2 = std::thread([&]() {
+		service.AddMessage("t2 Hello 8\n");
+		service.AddMessage("t2 Hello\n");
+		service.AddMessage("t2 Hello 7\n");
+		service.AddMessage("t2 Hello 6\n");
+		service.Process();
+	});
+
+	t1.join();
+	t2.join();
+
+	return -EXIT_SUCCESS;
+}
---
+++ AuthService/tests/testmqttclient.h
@@ -0,0 +1,21 @@
+/*
+ * Mocked MQTT client.
+ *
+ * @author Michel Megens
+ * @email  michel@michelmegens.net
+ */
+
+#pragma once
+
+#include <sensateiot/imqttclient.h>
+
+namespace sensateiot::test
+{
+	class TestMqttClient : public mqtt::IMqttClient {
+
+	public:
+		void Connect(const config::Mqtt &mqtt) override
+		{
+		}
+	};
+}
