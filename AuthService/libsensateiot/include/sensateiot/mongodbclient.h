a8f0a430cabce3367b2ab2406e639eb74bc71b46
Michel Megens <michel@michelmegens.net>
2020-05-01 01:22:54 +0200

    auth-service: mongodb: add thread-safety
    Implement double lock checking in order to guarantee singleton
    thread-safety across all supported platforms.
    
    Signed-off-by: Michel Megens <michel@michelmegens.net>

--- AuthService/libsensateiot/include/sensateiot/mongodbclient.h
+++ AuthService/libsensateiot/include/sensateiot/mongodbclient.h
@@ -12,6 +12,10 @@
 
 #include <mongocxx/instance.hpp>
 #include <mongocxx/pool.hpp>
+#include <mongocxx/client.hpp>
+
+#include <atomic>
+#include <mutex>
 
 namespace sensateiot::util
 {
@@ -19,14 +23,22 @@ namespace sensateiot::util
 	public:
 		static void Init(config::MongoDB config);
 		static MongoDBClient& GetClient();
+		static void Destroy();
+
+		typedef mongocxx::pool::entry PoolClient;
+		typedef mongocxx::client& ClientReference;
+
+		PoolClient acquire();
 
 	private:
-		explicit MongoDBClient(config::MongoDB config);
+		explicit MongoDBClient(const config::MongoDB& config);
 
 		mongocxx::instance m_instance{};
 		mongocxx::uri m_uri{};
 		mongocxx::pool m_pool;
 
 		static config::MongoDB Config;
+		static std::atomic<MongoDBClient*> Instance;
+		static std::mutex Lock;
 	};
 }
--- AuthService/libsensateiot/util/mongodbclient.cpp
+++ AuthService/libsensateiot/util/mongodbclient.cpp
@@ -12,10 +12,11 @@
 
 namespace sensateiot::util
 {
-	MongoDBClient::MongoDBClient(config::MongoDB config) :
+	MongoDBClient::MongoDBClient(const config::MongoDB& config) :
 		m_uri(config.GetConnectionString()), m_pool(m_uri)
 	{
 		auto testClient = this->m_pool.acquire();
+
 		auto& lg = util::Log::GetLog();
 
 		if(testClient) {
@@ -28,11 +29,38 @@ namespace sensateiot::util
 		Config = std::move(config);
 	}
 
-	MongoDBClient &MongoDBClient::GetClient()
+	MongoDBClient& MongoDBClient::GetClient()
 	{
-		static MongoDBClient client(Config);
-		return client;
+		if(Instance == nullptr) {
+			std::lock_guard<std::mutex> l(Lock);
+
+			if(Instance == nullptr) {
+				Instance = new MongoDBClient(Config);
+			}
+		}
+
+		return *Instance;
 	}
 
+	MongoDBClient::PoolClient MongoDBClient::acquire()
+	{
+		return this->m_pool.acquire();
+	}
+
+	void MongoDBClient::Destroy()
+	{
+		if(Instance != nullptr) {
+			std::lock_guard<std::mutex> l(Lock);
+
+			if(Instance != nullptr) {
+				auto *ptr = Instance.load();
+				delete ptr;
+				Instance = nullptr;
+			}
+		}
+	}
+
+	std::atomic<MongoDBClient*> MongoDBClient::Instance { nullptr };
+	std::mutex MongoDBClient::Lock;
 	config::MongoDB MongoDBClient::Config;
 }
