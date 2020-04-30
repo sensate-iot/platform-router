0de3d1fc6c5462db386a02558e95594f97806112
Michel Megens <michel@michelmegens.net>
2020-04-30 22:30:17 +0200

    auth-service: update project configuration
    Update the Authorization Service project configuration.
    
    Signed-off-by: Michel Megens <michel@michelmegens.net>

--- AuthService/conanfile.py
+++ AuthService/conanfile.py
@@ -9,16 +9,26 @@ import os
 class AuthService(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
+    default_options = {
+        "libpq:with_openssl": False,
+        "paho-mqtt-cpp:ssl": True,
+        "paho-mqtt-c:ssl": True,
+        "libpq:shared": False,
+        "libpqxx:shared": False,
+        "mongo-cxx-driver:shared": True
+    }
+
+    def configure(self):
+        if not self.is_windows():
+            return
+
+        self.default_options['mongo-cxx-driver:shared'] = False
 
     def is_windows(self):
         return os.name == 'nt'
 
     def requirements(self):
         if self.is_windows():
-            self.default_options = {
-                "libpq:with_openssl": False
-            }
-
             self.requires("mongo-cxx-driver/3.3.0@bincrafters/stable")
             self.requires("mongo-c-driver/1.16.1@bincrafters/stable", override=True)
             self.requires("paho-mqtt-cpp/1.1", override=False)
@@ -27,24 +37,20 @@ class AuthService(ConanFile):
             self.requires("boost_log/1.67.0@bincrafters/stable")
             self.requires("zlib/1.2.11", override=True)
         else:
-            self.default_options = {
-                "libpq:with_openssl": False,
-                "paho-mqtt-cpp:ssl": True,
-                "libpq:shared": False,
-                "libpqxx:shared": False
-            }
-
             self.requires("mongo-cxx-driver/3.3.0@bincrafters/stable")
             self.requires("mongo-c-driver/1.16.1@bincrafters/stable", override=True)
             self.requires("boost_log/1.67.0@bincrafters/stable")
             self.requires("zlib/1.2.11", override=True)
             self.requires("paho-mqtt-cpp/1.1", override=False)
+            self.requires("paho-mqtt-c/1.3.1", override=True)
             self.requires("libpq/11.5", override=True)
             self.requires("libpqxx/7.0.5", override=False)
-            self.requires("openssl/1.1.1f", override=False)
+            self.requires("openssl/1.1.1f", override=True)
 
     def imports(self):
-        self.copy('libpq.so*', '', 'lib')
+        self.copy('libpq.so*', 'libdist', 'lib')
+        self.copy('lib*mongo*.so*', 'libdist', 'lib')
+        self.copy('lib*bson*.so*', 'libdist', 'lib')
 
     def build(self):
         cmake = CMake(self)
