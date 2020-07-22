3e94a539571d38f1b9357ad3c6e0100075f84e56
Michel Megens <michel@michelmegens.net>
2020-07-22 18:29:05 +0200

    auth-service: messages: add message validator and proto's
    Add proto files for text messages and a stub to validate incoming text
    messages.
    
    Signed-off-by: Michel Megens <michel@michelmegens.net>

--- AuthService/libsensateiot/include/sensateiot/data/messagevalidator.h
+++ AuthService/libsensateiot/include/sensateiot/data/messagevalidator.h
@@ -6,3 +6,16 @@
  */
 
 #pragma once
+
+#include <string>
+#include <vector>
+#include <utility>
+
+#include <sensateiot/models/message.h>
+
+namespace sensateiot::data
+{
+	struct MessageValidator {
+		std::pair<bool, models::Message> operator()(const std::string& str) const;
+	};
+}
