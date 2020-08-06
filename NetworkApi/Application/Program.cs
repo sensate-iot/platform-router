680341b18f011ea3c24434767a4bc7c13b23ccb3
Michel Megens <michel@michelmegens.net>
2020-08-06 23:40:40 +0200

    network-api: update application settings
    Stop using app secrets in development mode and use the appsettings.json
    file instead.
    
    Signed-off-by: Michel Megens <michel@michelmegens.net>

--- NetworkApi/Application/Program.cs
+++ NetworkApi/Application/Program.cs
@@ -43,10 +43,7 @@ namespace SensateService.NetworkApi.Application
 				.UseConfiguration(conf)
 				.UseContentRoot(Directory.GetCurrentDirectory())
 				.ConfigureAppConfiguration((hostingContext, config) => {
-					if(hostingContext.HostingEnvironment.IsProduction())
-						config.AddJsonFile(GetAppSettings(), optional: false, reloadOnChange: true);
-					else
-						config.AddUserSecrets<Startup>();
+					config.AddJsonFile(GetAppSettings(), optional: false, reloadOnChange: true);
 					config.AddEnvironmentVariables();
 				})
 				.ConfigureLogging((hostingContext, logging) => {
