b6f0a3f40783e8fb2c5e9a377b90f66644c96be7
Michel Megens <michel@michelmegens.net>
2020-08-07 09:44:32 +0200

    auth-service: zip: remove unimplemented definitions
    Remove unimplemented function definitions.
    
    Signed-off-by: Michel Megens <michel@michelmegens.net>

--- AuthService/libsensateiot/include/sensateiot/util/gzip.h
+++ AuthService/libsensateiot/include/sensateiot/util/gzip.h
@@ -14,7 +14,5 @@
 
 namespace sensateiot::util
 {
-	//extern DLL_EXPORT std::string Compress(const std::string& data);
 	extern DLL_EXPORT std::string Compress(const std::vector<char>& data);
-	extern DLL_EXPORT std::string Decompress(const std::string& data);
 }
