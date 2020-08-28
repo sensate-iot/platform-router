5ee1f26ebed232f1a9fdf7da518327df0616914d
Michel Megens <Michel.Megens@cm.com>
2020-08-28 15:39:48 +0200

    caching: distributed: implement redis caching
    Distributed caching can be implement in roughly 3 ways:
    
    	* Using an external memory store;
    	* Using SQL;
    	* Own protocols.
    
    The first distributed driver implements the first option using Redis as
    a caching backend.
    
    Signed-off-by: Michel Megens <Michel.Megens@cm.com>

---
+++ Common/SensateService.Common.Caching/Abstract/IDistributedCache.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * Distributed redis cache.
+ *
+ * @author Michel Megens
+ * @email  michel@michelmegens.net
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SensateService.Common.Caching.Abstract
+{
+	public interface IDistributedCache<TValue> : IDisposable where TValue : class
+	{
+		Task SetAsync(string key, TValue value, CacheEntryOptions options = null, CancellationToken ct = default);
+		Task<TValue> GetAsync(string key, CancellationToken ct = default);
+		Task RemoveAsync(string key, CancellationToken ct = default);
+		Task SetRangeAsync(ICollection<KeyValuePair<string, TValue>> values, CacheEntryOptions options = null, CancellationToken ct = default);
+		Task<IEnumerable<TValue>> GetRangeAsync(ICollection<string> keys, CancellationToken ct = default);
+	}
+}
