2caf8183a52d57ff310ef6200043f82cf4069eef
Michel Megens <michel@michelmegens.net>
2020-11-10 11:58:19 +0100

    migrations: trigger: remove trigger table
    Remove the Sensate IoT trigger tables in anticipation of the new
    networking services.
    
    Signed-off-by: Michel Megens <michel@michelmegens.net>

---
+++ Common/SensateService.Common.SqlSetup/Migrations/20201109231636_DropTriggerTables.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SensateService.SqlSetup.Migrations
+{
+    public partial class DropTriggerTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+	        migrationBuilder.DropTable("TriggerInvocations");
+	        migrationBuilder.DropTable("TriggerActions");
+	        migrationBuilder.DropTable("Triggers");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
