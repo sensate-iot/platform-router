288393a1f056f69f916b4dfed91deafa31382f2e
Michel Megens <michel@michelmegens.net>
2020-11-11 20:37:43 +0100

    network: config: update namespaces
    Update the config API namespace.
    
    Signed-off-by: Michel Megens <michel@michelmegens.net>

--- SensateIoT.Platform/SensateIoT.Platform.Network/SensateIoT.Platform.Network.ConfigApi/Application/Program.cs
+++ SensateIoT.Platform/SensateIoT.Platform.Network/SensateIoT.Platform.Network.ConfigApi/Application/Program.cs
@@ -1,13 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
-namespace SensateIoT.Platform.Network.ConfigApi
+namespace SensateIoT.Platform.Network.ConfigApi.Application
 {
 	public class Program
 	{
