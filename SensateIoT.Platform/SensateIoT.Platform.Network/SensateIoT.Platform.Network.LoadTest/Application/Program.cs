adcd5c7d78e22f06842fc86fce3668c441972be7
Michel Megens <michel@michelmegens.net>
2020-11-11 20:51:22 +0100

    network: tests: add a load test project
    Add a project to load test various components of the system.
    
    Signed-off-by: Michel Megens <michel@michelmegens.net>

---
+++ SensateIoT.Platform/SensateIoT.Platform.Network/SensateIoT.Platform.Network.LoadTest/Application/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * Load test entry point.
+ *
+ * @author Michel Megens
+ * @email  michel@michelmegens.net
+ */
+
+using System;
+using SensateIoT.Platform.Network.LoadTest.CacheTests;
+
+namespace SensateIoT.Platform.Network.LoadTest.Application
+{
+	public class Program
+	{
+		private const int ReadTestSize = 1000000;
+
+		private static void RunMemoryTests()
+		{
+			var reads = new ReadTests();
+			reads.TestSynchronousRead(ReadTestSize);
+			Console.WriteLine();
+			Console.WriteLine("------------------------");
+			Console.WriteLine();
+
+			reads.TestAsynchronousRead(ReadTestSize).Wait();
+		}
+
+		private static void RunDistributedTests()
+		{
+			//var tests = new GetSetTests();
+			//tests.Run().Wait();
+		}
+
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Starting load tests for the caching subsystem...");
+
+			RunMemoryTests();
+			RunDistributedTests();
+#if !DEBUG
+			Console.ReadLine();
+#endif
+		}
+	}
+}
