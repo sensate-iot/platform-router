b00002118b0251d3c25ee37b664259c8baf695ec
Michel Megens <michel@michelmegens.net>
2020-11-12 22:44:37 +0100

    network: data: add api key and account definitions
    Implement the `Account' and `ApiKey' DTO models.
    
    Signed-off-by: Michel Megens <michel@michelmegens.net>

--- SensateIoT.Platform/SensateIoT.Platform.Network/SensateIoT.Platform.Network.Data/DTO/Account.cs
+++ SensateIoT.Platform/SensateIoT.Platform.Network/SensateIoT.Platform.Network.Data/DTO/Account.cs
@@ -5,10 +5,14 @@
  * @email  michel@michelmegens.net
  */
 
+using System;
+
 namespace SensateIoT.Platform.Network.Data.DTO
 {
 	public class Account
 	{
-
+		public Guid ID { get; set; }
+		public bool HasBillingLockout { get; set; }
+		public bool IsBanned { get; set; }
 	}
 }
--- SensateIoT.Platform/SensateIoT.Platform.Network/SensateIoT.Platform.Network.Data/DTO/ApiKey.cs
+++ SensateIoT.Platform/SensateIoT.Platform.Network/SensateIoT.Platform.Network.Data/DTO/ApiKey.cs
@@ -5,10 +5,15 @@
  * @email  michel@michelmegens.net
  */
 
+using System;
+
 namespace SensateIoT.Platform.Network.Data.DTO
 {
 	public class ApiKey
 	{
-
+		public string Key { get; set; }
+		public Guid AccountID { get; set; }
+		public bool IsRevoked { get; set; }
+		public bool IsReadOnly { get; set; }
 	}
 }
