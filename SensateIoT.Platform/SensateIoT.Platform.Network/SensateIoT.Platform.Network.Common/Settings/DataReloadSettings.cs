2d8427380f899fb8345a5cbc6c24eea7416ba1e9
Michel Megens <michel@michelmegens.net>
2020-11-13 14:27:40 +0100

    network: common: add data reload settings
    Signed-off-by: Michel Megens <michel.megens@cm.com>

---
+++ SensateIoT.Platform/SensateIoT.Platform.Network/SensateIoT.Platform.Network.Common/Settings/DataReloadSettings.cs
@@ -0,0 +1,17 @@
+﻿/*
+ * DataCache reload settings.
+ *
+ * @author Michel Megens
+ * @email  michel@michelmegens.net
+ */
+
+using System;
+
+namespace SensateIoT.Platform.Network.Common.Settings
+{
+	public class DataReloadSettings
+	{
+		public TimeSpan ReloadInterval { get; set; }
+		public TimeSpan StartDelay { get; set; }
+	}
+}
