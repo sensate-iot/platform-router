29fb8e696cc6ebd8f8d8386d13be3a3072153948
Michel Megens <michel@michelmegens.net>
2020-11-13 16:00:38 +0100

    network: services: implement a sensor reload service
    Implement a service that will reload all sensor data on a set interval.
    
    Signed-off-by: Michel Megens <michel.megens@cm.com>

--- SensateIoT.Platform/SensateIoT.Platform.Network/SensateIoT.Platform.Network.Common/Services/Background/TimedBackgroundService.cs
+++ SensateIoT.Platform/SensateIoT.Platform.Network/SensateIoT.Platform.Network.Common/Services/Background/TimedBackgroundService.cs
@@ -5,6 +5,7 @@
  * @email  michel@michelmegens.net
  */
 
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -15,10 +16,10 @@ namespace SensateIoT.Platform.Network.Common.Services.Background
 		private Timer _timer;
 		private long _millis;
 
-		private readonly int m_startDelay;
-		private readonly int m_interval;
+		private readonly TimeSpan m_startDelay;
+		private readonly TimeSpan m_interval;
 
-		protected TimedBackgroundService(int startDelay, int interval)
+		protected TimedBackgroundService(TimeSpan startDelay, TimeSpan interval)
 		{
 			this.m_startDelay = startDelay;
 			this.m_interval = interval;
@@ -40,7 +41,7 @@ namespace SensateIoT.Platform.Network.Common.Services.Background
 
 		public void Invoke(object arg)
 		{
-			Interlocked.Add(ref this._millis, this.m_interval);
+			Interlocked.Add(ref this._millis, Convert.ToInt64(this.m_interval.TotalMilliseconds));
 			var task = Task.Run(async () => { await this.ExecuteAsync(this.m_stoppingCts.Token); });
 			task.Wait();
 		}
---
+++ SensateIoT.Platform/SensateIoT.Platform.Network/SensateIoT.Platform.Network.Common/Services/Data/SensorReloadService.cs
@@ -0,0 +1,71 @@
+﻿/*
+ * Reload service for sensors.
+ *
+ * @author Michel Megens
+ * @email  michel@michelmegens.net
+ */
+
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+using SensateIoT.Platform.Network.Common.Caching.Object;
+using SensateIoT.Platform.Network.Common.Services.Background;
+using SensateIoT.Platform.Network.Common.Settings;
+using SensateIoT.Platform.Network.Data.DTO;
+using SensateIoT.Platform.Network.DataAccess.Repositories;
+
+namespace SensateIoT.Platform.Network.Common.Services.Data
+{
+	public class SensorReloadService : TimedBackgroundService
+	{
+		private readonly IServiceProvider m_provider;
+		private readonly IDataCache m_cache;
+		private readonly ILogger<SensorReloadService> m_logger;
+
+		public SensorReloadService(IServiceProvider provider,
+								   IDataCache cache,
+								   IOptions<DataReloadSettings> settings,
+								   ILogger<SensorReloadService> logger) : base(settings.Value.StartDelay, settings.Value.ReloadInterval)
+		{
+			this.m_provider = provider;
+			this.m_cache = cache;
+			this.m_logger = logger;
+		}
+
+		public override async Task ExecuteAsync(CancellationToken token)
+		{
+			this.m_logger.LogInformation("Starting sensor reload at {reloadStart}", DateTime.UtcNow);
+
+			using var scope = this.m_provider.CreateScope();
+			var sensorRepo = scope.ServiceProvider.GetRequiredService<ISensorRepository>();
+			var triggerRepo = scope.ServiceProvider.GetRequiredService<ITriggerRepository>();
+
+			var sw = Stopwatch.StartNew();
+			var sensorTask = sensorRepo.GetSensorsAsync(token);
+			var triggerTask = triggerRepo.GetTriggerInfoAsync(token);
+
+			await Task.WhenAll(sensorTask, triggerTask).ConfigureAwait(false);
+			var dict = sensorTask.Result.ToDictionary(k => k.ID, v => v);
+
+			foreach(var info in triggerTask.Result) {
+				var route = new SensorTrigger {
+					HasActions = info.ActionCount > 0,
+					IsTextTrigger = info.TextTrigger
+				};
+
+				dict[info.SensorID].TriggerInformation = route;
+			}
+
+			this.m_cache.Append(dict.Values);
+			sw.Stop();
+			this.m_logger.LogInformation("Finished sensor reload at {reloadEnd}. Reload took {duration}ms.", DateTime.UtcNow, sw.ElapsedMilliseconds);
+		}
+	}
+}
