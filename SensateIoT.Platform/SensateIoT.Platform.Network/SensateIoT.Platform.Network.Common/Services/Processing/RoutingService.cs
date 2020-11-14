524f7e0f1843042140e4a50287c70ee376741c25
Michel Megens <michel@michelmegens.net>
2020-11-14 17:52:18 +0100

    network: routing: add a routing service
    Add a stub for a routing service.
    
    Signed-off-by: Michel Megens <michel@michelmegens.net>

---
+++ SensateIoT.Platform/SensateIoT.Platform.Network/SensateIoT.Platform.Network.Common/Services/Processing/RoutingService.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * Routing service.
+ *
+ * @author Michel Megens
+ * @email  michel@michelmegens.net
+ */
+
+using System.Threading;
+using System.Threading.Tasks;
+
+using SensateIoT.Platform.Network.Common.Services.Background;
+
+namespace SensateIoT.Platform.Network.Common.Services.Processing
+{
+	public class RoutingService : BackgroundService
+	{
+		/*
+		 * Route messages through the platform:
+		 *
+		 *		1 Check validity;
+		 *		2 Trigger routing;
+		 *		3 Live data routing;
+		 *		4 Forward to storage.
+		 */
+		public RoutingService()
+		{
+
+		}
+
+		public override async Task ExecuteAsync(CancellationToken token)
+		{
+			await Task.CompletedTask;
+		}
+	}
+}
