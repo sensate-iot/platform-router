6d9b7e285eff2161e05bc2f4aa595405c88244c1
Michel Megens <michel@michelmegens.net>
2020-11-14 01:11:43 +0100

    common: sql: update migration formatting
    Update the formatting of the drop trigger tables migration.
    
    Signed-off-by: Michel Megens <michel@michelmegens.net>

--- Common/SensateService.Common.SqlSetup/Migrations/20201109231636_DropTriggerTables.cs
+++ Common/SensateService.Common.SqlSetup/Migrations/20201109231636_DropTriggerTables.cs
@@ -2,18 +2,18 @@
 
 namespace SensateService.SqlSetup.Migrations
 {
-    public partial class DropTriggerTables : Migration
-    {
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-	        migrationBuilder.DropTable("TriggerInvocations");
-	        migrationBuilder.DropTable("TriggerActions");
-	        migrationBuilder.DropTable("Triggers");
-        }
+	public partial class DropTriggerTables : Migration
+	{
+		protected override void Up(MigrationBuilder migrationBuilder)
+		{
+			migrationBuilder.DropTable("TriggerInvocations");
+			migrationBuilder.DropTable("TriggerActions");
+			migrationBuilder.DropTable("Triggers");
+		}
 
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
+		protected override void Down(MigrationBuilder migrationBuilder)
+		{
 
-        }
-    }
+		}
+	}
 }
