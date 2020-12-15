cd16655bed9ef94af99b4314211cd7276ff28b19
Michel Megens <michel@michelmegens.net>
2020-12-15 22:14:42 +0100

    common: sql: update code style
    Update code formatting.
    
    Signed-off-by: Michel Megens <michel@michelmegens.net>

--- Common/SensateService.Common.SqlSetup/Migrations/20201214204157_DropSensorLinksTable.cs
+++ Common/SensateService.Common.SqlSetup/Migrations/20201214204157_DropSensorLinksTable.cs
@@ -2,16 +2,16 @@
 
 namespace SensateService.SqlSetup.Migrations
 {
-    public partial class DropSensorLinksTable : Migration
-    {
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
+	public partial class DropSensorLinksTable : Migration
+	{
+		protected override void Up(MigrationBuilder migrationBuilder)
+		{
 			migrationBuilder.DropTable("SensorLinks");
-        }
+		}
 
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
+		protected override void Down(MigrationBuilder migrationBuilder)
+		{
 
-        }
-    }
+		}
+	}
 }
