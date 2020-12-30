6ad1ed768ead276cecd0ff819707ca4cdcd414cf
Michel Megens <michel@michelmegens.net>
2020-12-30 18:01:59 +0100

    network: data-access: fix sensor key creation
    Fix the data access layer to correctly create a new sensor key using the
    correct stored procedure.
    
    Signed-off-by: Michel Megens <michel.megens@cm.com>

--- SensateIoT.Platform/SensateIoT.Platform.Network/SensateIoT.Platform.Network.DataAccess/Repositories/ApiKeyRepository.cs
+++ SensateIoT.Platform/SensateIoT.Platform.Network/SensateIoT.Platform.Network.DataAccess/Repositories/ApiKeyRepository.cs
@@ -102,8 +102,10 @@ namespace SensateIoT.Platform.Network.DataAccess.Repositories
 			cmd.CommandText = NetworkApi_CreateSensorKey;
 
 			var key = new NpgsqlParameter("key", NpgsqlDbType.Text) { Value = sensor.Secret };
+			var uid = new NpgsqlParameter("userid", NpgsqlDbType.Text) { Value = sensor.Owner };
 
 			cmd.Parameters.Add(key);
+			cmd.Parameters.Add(uid);
 
 			await using var reader = await cmd.ExecuteReaderAsync(ct).ConfigureAwait(false);
 
