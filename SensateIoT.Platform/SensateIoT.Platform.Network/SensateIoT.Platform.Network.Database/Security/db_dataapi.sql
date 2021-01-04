3242b4dd02edcac15fbc444843a37569b9bc7845
Michel Megens <michel@michelmegens.net>
2021-01-04 17:42:43 +0100

    network: database: setup data api users/roles
    Setup security rules for the data API:
    
    	* Add role db_dataapi;
    	* Add user sql_dataapi.
    
    Signed-off-by: Michel Megens <michel.megens@cm.com>

---
+++ SensateIoT.Platform/SensateIoT.Platform.Network/SensateIoT.Platform.Network.Database/Security/db_dataapi.sql
@@ -0,0 +1,11 @@
+CREATE ROLE db_dataapi;
+
+GRANT EXECUTE ON FUNCTION dataapi_selectinvocationcount(TEXT) TO db_dataapi;
+GRANT EXECUTE ON FUNCTION dataapi_selectsensorlinkbysensorid(VARCHAR(24)) TO db_dataapi;
+GRANT EXECUTE ON FUNCTION dataapi_selectsensorlinkbyuserid(UUID) TO db_dataapi;
+GRANT EXECUTE ON FUNCTION dataapi_selectsensorlinkcountbyuserid(UUID) TO db_dataapi;
+
+GRANT SELECT ON TABLE "TriggerActions" TO db_triggerservice;
+GRANT SELECT ON TABLE "Triggers" TO db_triggerservice;
+GRANT SELECT ON TABLE "SensorLinks" TO db_dataapi;
+GRANT SELECT ON TABLE "TriggerInvocations" TO db_triggerservice;
