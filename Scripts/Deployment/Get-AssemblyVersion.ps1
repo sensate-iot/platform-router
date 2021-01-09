0becdb9faef5c08484117f16fccc775fc60d0a47
Michel Megens <michel@michelmegens.net>
2021-01-10 00:23:02 +0100

    project: ci/cd: update the github workflows
    Add workflows for:
    
    	- the MQTT service;
    	- fixed the trigger service workflow.
    
    Signed-off-by: Michel Megens <michel@michelmegens.net>

--- Scripts/Deployment/Get-AssemblyVersion.ps1
+++ Scripts/Deployment/Get-AssemblyVersion.ps1
@@ -1,6 +1,19 @@
+#
+# Get the assembly version from an XML file.
+#
+# @author Michel Megens
+# @email  michel@michelmegens.net
+#
 
 $content = Get-Content $args[0]
-
 $xml = [Xml] $content
-$version = $xml.Project.PropertyGroup.AssemblyVersion
+$assemblyVersion = [string] $xml.Project.PropertyGroup.AssemblyVersion
+$length = $assemblyVersion.Length
+
+if($length -Lt 8) {
+    $version = $assemblyVersion;
+} else {
+    $version = $assemblyVersion.Substring(0, $length - 3)
+}
+
 echo $version
