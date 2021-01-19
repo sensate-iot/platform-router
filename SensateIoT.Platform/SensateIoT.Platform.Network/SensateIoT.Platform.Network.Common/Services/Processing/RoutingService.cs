5e87001893c23c3bac9e7aecd7798cfecf355718
Michel Megens <michel@michelmegens.net>
2021-01-19 16:50:53 +0100

    network: routing: implement a configurable interval
    Let the message router run on the same interval as the message
    publication service.
    
    Signed-off-by: Michel Megens <michel@michelmegens.net>

--- SensateIoT.Platform/SensateIoT.Platform.Network/SensateIoT.Platform.Network.Common/Services/Processing/RoutingService.cs
+++ SensateIoT.Platform/SensateIoT.Platform.Network/SensateIoT.Platform.Network.Common/Services/Processing/RoutingService.cs
@@ -82,7 +82,7 @@ namespace SensateIoT.Platform.Network.Common.Services.Processing
 			do {
 				if(this.m_messages.Count <= 0) {
 					try {
-						await Task.Delay(TimeSpan.FromMilliseconds(250), token);
+						await Task.Delay(this.m_settings.InternalInterval, token);
 					} catch(OperationCanceledException) {
 						this.m_logger.LogWarning("Routing task cancelled.");
 					}
