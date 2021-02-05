512ceecfaec65a259a3d6cec2e67fd90cae5c794
Michel Megens <michel@michelmegens.net>
2021-02-05 19:46:16 +0100

    network: router: implement ingress router metrics
    Add request duraiton metrics to the ingress router. These metrics are
    exposed to prometheus using the default metrics endpoint.
    
    Signed-off-by: Michel Megens <michel@michelmegens.net>

--- SensateIoT.Platform/SensateIoT.Platform.Network/SensateIoT.Platform.Network.Router/Services/IngressRouter.cs
+++ SensateIoT.Platform/SensateIoT.Platform.Network/SensateIoT.Platform.Network.Router/Services/IngressRouter.cs
@@ -29,6 +29,7 @@ namespace SensateIoT.Platform.Network.Router.Services
 		private readonly ILogger<IngressRouter> m_logger;
 		private readonly Counter m_measurementRequests;
 		private readonly Counter m_messageRequests;
+		private readonly Histogram m_duration;
 
 		public IngressRouter(IMessageQueue queue, ILogger<IngressRouter> logger)
 		{
@@ -36,37 +37,43 @@ namespace SensateIoT.Platform.Network.Router.Services
 			this.m_logger = logger;
 			this.m_measurementRequests = Metrics.CreateCounter("router_measurement_requests_total", "Total amount of measurement routing requests.");
 			this.m_messageRequests = Metrics.CreateCounter("router_message_requests_total", "Total amount of message routing requests.");
+			this.m_duration = Metrics.CreateHistogram("router_ingress_request_duration_seconds", "Histogram of egress routing duration.");
 		}
 
 		public override Task<RoutingResponse> EnqueueMeasurement(Contracts.DTO.Measurement request, ServerCallContext context)
 		{
 			RoutingResponse response;
-			this.m_logger.LogDebug("Received measurement routing request from sensor {sensorId}.", request.SensorID);
 
-			try {
-				var count = 0;
-				var dto = MeasurementProtobufConverter.Convert(request);
+			using(this.m_duration.NewTimer()) {
+				this.m_logger.LogDebug("Received measurement routing request from sensor {sensorId}.",
+				                       request.SensorID);
 
-				if(MeasurementValidator.Validate(dto)) {
-					this.m_queue.Add(dto);
-					this.m_measurementRequests.Inc();
+				try {
+					var count = 0;
+					var dto = MeasurementProtobufConverter.Convert(request);
 
-					count += 1;
+					if(MeasurementValidator.Validate(dto)) {
+						this.m_queue.Add(dto);
+						this.m_measurementRequests.Inc();
+
+						count += 1;
+					}
+
+					response = new RoutingResponse {
+						Count = count,
+						Message = "Measurements queued.",
+						ResponseID = ByteString.CopyFrom(Guid.NewGuid().ToByteArray())
+					};
+				} catch(FormatException) {
+					this.m_logger.LogWarning("Received messages from an invalid sensor (ID): {sensorID}",
+					                         request.SensorID);
+
+					response = new RoutingResponse {
+						Count = 0,
+						Message = "Measurements not queued. Invalid sensor ID.",
+						ResponseID = ByteString.CopyFrom(Guid.NewGuid().ToByteArray())
+					};
 				}
-
-				response = new RoutingResponse {
-					Count = count,
-					Message = "Measurements queued.",
-					ResponseID = ByteString.CopyFrom(Guid.NewGuid().ToByteArray())
-				};
-			} catch(FormatException) {
-				this.m_logger.LogWarning("Received messages from an invalid sensor (ID): {sensorID}", request.SensorID);
-
-				response = new RoutingResponse {
-					Count = 0,
-					Message = "Measurements not queued. Invalid sensor ID.",
-					ResponseID = ByteString.CopyFrom(Guid.NewGuid().ToByteArray())
-				};
 			}
 
 			return Task.FromResult(response);
@@ -75,31 +82,35 @@ namespace SensateIoT.Platform.Network.Router.Services
 		public override Task<RoutingResponse> EnqueueMessage(Contracts.DTO.TextMessage request, ServerCallContext context)
 		{
 			RoutingResponse response;
-			this.m_logger.LogDebug("Received message routing request from sensor {sensorId}.", request.SensorID);
 
-			try {
-				var count = 0;
-				var dto = MessageProtobufConverter.Convert(request);
+			using(this.m_duration.NewTimer()) {
+				this.m_logger.LogDebug("Received message routing request from sensor {sensorId}.", request.SensorID);
 
-				if(MessageValidator.Validate(dto)) {
-					this.m_queue.Add(dto);
-					this.m_messageRequests.Inc();
-					count += 1;
+				try {
+					var count = 0;
+					var dto = MessageProtobufConverter.Convert(request);
+
+					if(MessageValidator.Validate(dto)) {
+						this.m_queue.Add(dto);
+						this.m_messageRequests.Inc();
+						count += 1;
+					}
+
+					response = new RoutingResponse {
+						Count = count,
+						Message = "Message queued.",
+						ResponseID = ByteString.CopyFrom(Guid.NewGuid().ToByteArray())
+					};
+				} catch(FormatException) {
+					this.m_logger.LogWarning("Received messages from an invalid sensor (ID): {sensorID}",
+					                         request.SensorID);
+
+					response = new RoutingResponse {
+						Count = 0,
+						Message = "Messages not queued. Invalid sensor ID.",
+						ResponseID = ByteString.CopyFrom(Guid.NewGuid().ToByteArray())
+					};
 				}
-
-				response = new RoutingResponse {
-					Count = count,
-					Message = "Message queued.",
-					ResponseID = ByteString.CopyFrom(Guid.NewGuid().ToByteArray())
-				};
-			} catch(FormatException) {
-				this.m_logger.LogWarning("Received messages from an invalid sensor (ID): {sensorID}", request.SensorID);
-
-				response = new RoutingResponse {
-					Count = 0,
-					Message = "Messages not queued. Invalid sensor ID.",
-					ResponseID = ByteString.CopyFrom(Guid.NewGuid().ToByteArray())
-				};
 			}
 
 			return Task.FromResult(response);
@@ -108,35 +119,38 @@ namespace SensateIoT.Platform.Network.Router.Services
 		public override Task<RoutingResponse> EnqueueBulkMeasurements(Contracts.DTO.MeasurementData request, ServerCallContext context)
 		{
 			RoutingResponse response;
-			this.m_logger.LogDebug("Bulk ingress request with {count} measurements.", request.Measurements.Count);
 
-			try {
-				var dto = new List<Measurement>();
+			using(this.m_duration.NewTimer()) {
+				this.m_logger.LogDebug("Bulk ingress request with {count} measurements.", request.Measurements.Count);
 
-				foreach(var raw in request.Measurements) {
-					var measurement = MeasurementProtobufConverter.Convert(raw);
+				try {
+					var dto = new List<Measurement>();
 
-					if(MeasurementValidator.Validate(measurement)) {
-						dto.Add(measurement);
+					foreach(var raw in request.Measurements) {
+						var measurement = MeasurementProtobufConverter.Convert(raw);
+
+						if(MeasurementValidator.Validate(measurement)) {
+							dto.Add(measurement);
+						}
 					}
+
+					this.m_queue.AddRange(dto);
+					this.m_measurementRequests.Inc();
+
+					response = new RoutingResponse {
+						Count = dto.Count,
+						Message = "Messages queued.",
+						ResponseID = ByteString.CopyFrom(Guid.NewGuid().ToByteArray())
+					};
+				} catch(FormatException ex) {
+					this.m_logger.LogWarning("Received messages from an invalid sensor. Exception: {exception}", ex);
+
+					response = new RoutingResponse {
+						Count = 0,
+						Message = "Messages not queued. Invalid sensor ID.",
+						ResponseID = ByteString.CopyFrom(Guid.NewGuid().ToByteArray())
+					};
 				}
-
-				this.m_queue.AddRange(dto);
-				this.m_measurementRequests.Inc();
-
-				response = new RoutingResponse {
-					Count = dto.Count,
-					Message = "Messages queued.",
-					ResponseID = ByteString.CopyFrom(Guid.NewGuid().ToByteArray())
-				};
-			} catch(FormatException ex) {
-				this.m_logger.LogWarning("Received messages from an invalid sensor. Exception: {exception}", ex);
-
-				response = new RoutingResponse {
-					Count = 0,
-					Message = "Messages not queued. Invalid sensor ID.",
-					ResponseID = ByteString.CopyFrom(Guid.NewGuid().ToByteArray())
-				};
 			}
 
 			return Task.FromResult(response);
@@ -145,35 +159,38 @@ namespace SensateIoT.Platform.Network.Router.Services
 		public override Task<RoutingResponse> EnqueueBulkMessages(Contracts.DTO.TextMessageData request, ServerCallContext context)
 		{
 			RoutingResponse response;
-			this.m_logger.LogDebug("Bulk ingress request with {count} messages.", request.Messages.Count);
 
-			try {
-				var dto = new List<Message>();
+			using(this.m_duration.NewTimer()) {
+				this.m_logger.LogDebug("Bulk ingress request with {count} messages.", request.Messages.Count);
 
-				foreach(var raw in request.Messages) {
-					var message = MessageProtobufConverter.Convert(raw);
+				try {
+					var dto = new List<Message>();
 
-					if(MessageValidator.Validate(message)) {
-						dto.Add(message);
+					foreach(var raw in request.Messages) {
+						var message = MessageProtobufConverter.Convert(raw);
+
+						if(MessageValidator.Validate(message)) {
+							dto.Add(message);
+						}
 					}
+
+					this.m_queue.AddRange(dto);
+					this.m_messageRequests.Inc();
+
+					response = new RoutingResponse {
+						Count = dto.Count,
+						Message = "Messages queued.",
+						ResponseID = ByteString.CopyFrom(Guid.NewGuid().ToByteArray())
+					};
+				} catch(FormatException ex) {
+					this.m_logger.LogWarning("Received messages from an invalid sensor. Exception: {exception}", ex);
+
+					response = new RoutingResponse {
+						Count = 0,
+						Message = "Messages not queued. Invalid sensor ID.",
+						ResponseID = ByteString.CopyFrom(Guid.NewGuid().ToByteArray())
+					};
 				}
-
-				this.m_queue.AddRange(dto);
-				this.m_messageRequests.Inc();
-
-				response = new RoutingResponse {
-					Count = dto.Count,
-					Message = "Messages queued.",
-					ResponseID = ByteString.CopyFrom(Guid.NewGuid().ToByteArray())
-				};
-			} catch(FormatException ex) {
-				this.m_logger.LogWarning("Received messages from an invalid sensor. Exception: {exception}", ex);
-
-				response = new RoutingResponse {
-					Count = 0,
-					Message = "Messages not queued. Invalid sensor ID.",
-					ResponseID = ByteString.CopyFrom(Guid.NewGuid().ToByteArray())
-				};
 			}
 
 			return Task.FromResult(response);
