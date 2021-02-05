ea35e179a9b36caedb59997235abd632534489e7
Michel Megens <michel@michelmegens.net>
2021-02-05 20:03:10 +0100

    network: common: correctly convert message's
    Convert the message encoding the when converting messages from the
    protobuf format to a database model.
    
    Signed-off-by: Michel Megens <michel@michelmegens.net>

--- SensateIoT.Platform/SensateIoT.Platform.Network/SensateIoT.Platform.Network.Common/Converters/MessageDatabaseConverter.cs
+++ SensateIoT.Platform/SensateIoT.Platform.Network/SensateIoT.Platform.Network.Common/Converters/MessageDatabaseConverter.cs
@@ -7,15 +7,17 @@
 
 using System.Collections.Generic;
 using System.Linq;
+
 using MongoDB.Bson;
 using MongoDB.Driver.GeoJsonObjectModel;
 
 using SensateIoT.Platform.Network.Contracts.DTO;
+using SensateIoT.Platform.Network.Data.Abstract;
 using SensateIoT.Platform.Network.Data.Models;
 
 namespace SensateIoT.Platform.Network.Common.Converters
 {
-	public class MessageDatabaseConverter
+	public static class MessageDatabaseConverter
 	{
 		public static IEnumerable<Message> Convert(TextMessageData messages)
 		{
@@ -24,9 +26,9 @@ namespace SensateIoT.Platform.Network.Common.Converters
 				Timestamp = msg.Timestamp.ToDateTime(),
 				PlatformTimestamp = msg.PlatformTime.ToDateTime(),
 				SensorId = ObjectId.Parse(msg.SensorID),
-				Location = new GeoJsonPoint<GeoJson2DGeographicCoordinates>(new GeoJson2DGeographicCoordinates(msg.Longitude, msg.Latitude))
-			})
-				.ToList();
+				Location = new GeoJsonPoint<GeoJson2DGeographicCoordinates>(new GeoJson2DGeographicCoordinates(msg.Longitude, msg.Latitude)),
+				Encoding = (MessageEncoding) msg.Encoding
+			}).ToList();
 		}
 	}
 }
