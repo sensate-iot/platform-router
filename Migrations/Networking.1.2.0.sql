5efa02dcd6d55079189e50e0abb5b98a9f85f172
Michel Megens <michel@michelmegens.net>
2021-03-10 23:25:30 +0100

    db: migrations: fix the networking db 1.2.0 migration
    Fix the `DOP FUNCTION' statement in the Networking DB migration for
    version 1.2.0.
    
    Signed-off-by: Michel Megens <michel@michelmegens.net>

--- Migrations/Networking.1.2.0.sql
+++ Migrations/Networking.1.2.0.sql
@@ -1,32 +1,4 @@
-DROP FUNCTION networkapi_createinvocation(
-    triggerid BIGINT,
-    actionid BIGINT,
-    timestmp TIMESTAMP
-)
-    RETURNS TABLE(
-        "ID" BIGINT,
-        "TriggerID" BIGINT,
-        "ActionID" BIGINT,
-        "Timestamp" TIMESTAMP
-    )
-    LANGUAGE plpgsql
-AS $$
-BEGIN
-	RETURN QUERY
-
-    INSERT INTO "TriggerInvocations" ("TriggerID",
-                                      "ActionID",
-                                      "Timestamp")
-    VALUES (triggerid,
-            actionid,
-            timestmp)
-    RETURNING
-        "TriggerInvocations"."ID",
-        "TriggerInvocations"."TriggerID",
-        "TriggerInvocations"."ActionID",
-        "TriggerInvocations"."Timestamp";
-END
-$$;
+DROP FUNCTION networkapi_createinvocation(triggerid BIGINT, actionid BIGINT, timestmp TIMESTAMP);
 
 CREATE FUNCTION triggerservice_createinvocation(
     triggerid BIGINT,
