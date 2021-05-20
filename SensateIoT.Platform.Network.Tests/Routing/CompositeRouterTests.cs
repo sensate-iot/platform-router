8419b3f2d902fd83ca7cc8453f1ef9c75a8cf4d9
Michel Megens <michel@michelmegens.net>
2021-05-20 22:40:39 +0200

    tests: routing: implement composite router tests
    Implement the test cache `CanExecuteRouters' for the composite router
    unit tests.
    
    Signed-off-by: Michel Megens <michel@michelmegens.net>

--- SensateIoT.Platform.Network.Tests/Routing/CompositeRouterTests.cs
+++ SensateIoT.Platform.Network.Tests/Routing/CompositeRouterTests.cs
@@ -1,13 +1,42 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using MongoDB.Bson;
+using Moq;
+
+using SensateIoT.Platform.Network.Common.Caching.Abstract;
+using SensateIoT.Platform.Network.Common.Collections.Abstract;
+using SensateIoT.Platform.Network.Common.Routing;
+using SensateIoT.Platform.Network.Data.DTO;
 
 namespace SensateIoT.Platform.Network.Tests.Routing
 {
 	[TestClass]
 	public class CompositeRouterTests
 	{
+		private static ObjectId SensorId = ObjectId.GenerateNewId();
+
 		[TestMethod]
 		public void CanExecuteRouters()
 		{
+			var router = CreateCompositeRouter();
+
+			var r1 = new RouterStub();
+			var r2 = new RouterStub();
+			var r3 = new RouterStub();
+
+			router.AddRouter(r1);
+			router.AddRouter(r2);
+			router.AddRouter(r3);
+
+			var msg = new Message {
+				SensorId = SensorId
+			};
+			router.Route(msg);
+
+			Assert.IsTrue(r1.Executed);
+			Assert.IsTrue(r2.Executed);
+			Assert.IsTrue(r3.Executed);
 		}
 
 		[TestMethod]
@@ -28,6 +57,23 @@ namespace SensateIoT.Platform.Network.Tests.Routing
 		[TestMethod]
 		public void EnqueuesNetworkEvents()
 		{
+		}
+
+		private static IRoutingCache CreateRoutingCache()
+		{
+			var cache = new Mock<IRoutingCache>();
+			var sensor = new Sensor {ID = SensorId};
+
+			cache.Setup(x => x[SensorId]).Returns(sensor);
+			return cache.Object;
+		}
+
+		private static CompositeRouter CreateCompositeRouter()
+		{
+			var logger = new Mock<ILogger<CompositeRouter>>();
+			var queue = new Mock<IRemoteNetworkEventQueue>();
+
+			return new CompositeRouter(CreateRoutingCache(), queue.Object, logger.Object);
 		}
 	}
 }
