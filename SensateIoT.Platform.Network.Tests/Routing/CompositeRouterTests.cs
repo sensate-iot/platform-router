2ba96738e41b937b2e517cd45eb4ea572f31fe00
Michel Megens <michel@michelmegens.net>
2021-05-22 18:08:10 +0200

    tests: improve router tests
    Improve the unit tests for the composite router.
    
    Signed-off-by: Michel Megens <michel@michelmegens.net>

--- SensateIoT.Platform.Network.Tests/Routing/CompositeRouterTests.cs
+++ SensateIoT.Platform.Network.Tests/Routing/CompositeRouterTests.cs
@@ -1,4 +1,13 @@
-﻿using Microsoft.Extensions.Logging;
+﻿/*
+ * Composite router unit tests.
+ *
+ * @author Michel Megens
+ * @email  michel@michelmegens.net
+ */
+
+using System;
+
+using Microsoft.Extensions.Logging;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using MongoDB.Bson;
@@ -6,7 +15,9 @@ using Moq;
 
 using SensateIoT.Platform.Network.Common.Caching.Abstract;
 using SensateIoT.Platform.Network.Common.Collections.Abstract;
+using SensateIoT.Platform.Network.Common.Exceptions;
 using SensateIoT.Platform.Network.Common.Routing;
+using SensateIoT.Platform.Network.Contracts.DTO;
 using SensateIoT.Platform.Network.Data.DTO;
 
 namespace SensateIoT.Platform.Network.Tests.Routing
@@ -42,21 +53,91 @@ namespace SensateIoT.Platform.Network.Tests.Routing
 		[TestMethod]
 		public void CanCancelRoutesGracefully()
 		{
+			var router = CreateCompositeRouter();
+
+			var r1 = new RouterStub();
+			var r2 = new RouterStub { Cancel = true };
+			var r3 = new RouterStub();
+
+			router.AddRouter(r1);
+			router.AddRouter(r2);
+			router.AddRouter(r3);
+
+			var msg = new Message {
+				SensorId = SensorId
+			};
+			router.Route(msg);
+
+			Assert.IsTrue(r1.Executed);
+			Assert.IsFalse(r2.Executed);
+			Assert.IsFalse(r3.Executed);
 		}
 
 		[TestMethod]
 		public void CanCatchRouterExceptions()
 		{
+			var router = CreateCompositeRouter();
+
+			var r1 = new RouterStub();
+			var r2 = new RouterStub { Exception = new RouterException("TestRouter", "testing exception catching.") };
+			var r3 = new RouterStub();
+
+			router.AddRouter(r1);
+			router.AddRouter(r2);
+			router.AddRouter(r3);
+
+			var msg = new Message {
+				SensorId = SensorId
+			};
+			router.Route(msg);
+
+			Assert.IsTrue(r1.Executed);
+			Assert.IsFalse(r2.Executed);
+			Assert.IsFalse(r3.Executed);
 		}
 
 		[TestMethod]
 		public void CannotCatchOtherExceptions()
 		{
+			var router = CreateCompositeRouter();
+
+			var r1 = new RouterStub();
+			var r2 = new RouterStub {Exception = new InvalidOperationException() };
+			var r3 = new RouterStub();
+
+			router.AddRouter(r1);
+			router.AddRouter(r2);
+			router.AddRouter(r3);
+
+			var msg = new Message {
+				SensorId = SensorId
+			};
+
+			Assert.ThrowsException<InvalidOperationException>(() => router.Route(msg));
 		}
 
 		[TestMethod]
 		public void EnqueuesNetworkEvents()
 		{
+			var count = 0;
+			var logger = new Mock<ILogger<CompositeRouter>>();
+			var queue = new Mock<IRemoteNetworkEventQueue>();
+
+			queue.Setup(x => x.EnqueueEvent(It.IsAny<NetworkEvent>())).Callback(() => count += 1);
+			var router = new CompositeRouter(CreateRoutingCache(), queue.Object, logger.Object);
+
+			var r1 = new RouterStub();
+			var r2 = new RouterStub();
+
+			router.AddRouter(r1);
+			router.AddRouter(r2);
+
+			var msg = new Message {
+				SensorId = SensorId
+			};
+
+			router.Route(msg);
+			Assert.AreEqual(1, count);
 		}
 
 		private static IRoutingCache CreateRoutingCache()
