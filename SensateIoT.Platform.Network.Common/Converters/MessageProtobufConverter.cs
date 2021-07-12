00ecc15afcab20551024a92e2c15263776982c2e
Michel Megens <michel@michelmegens.net>
2021-07-13 01:06:07 +0200

    project: remove unused code
    Signed-off-by: Michel Megens <michel@michelmegens.net>

--- SensateIoT.Platform.Network.Common/Converters/MessageProtobufConverter.cs
+++ SensateIoT.Platform.Network.Common/Converters/MessageProtobufConverter.cs
@@ -13,7 +13,6 @@ using Google.Protobuf.WellKnownTypes;
 using MongoDB.Bson;
 
 using SensateIoT.Platform.Network.Contracts.DTO;
-using SensateIoT.Platform.Network.Data.Abstract;
 using SensateIoT.Platform.Network.Data.DTO;
 using SensateIoT.Platform.Network.Data.Enums;
 
