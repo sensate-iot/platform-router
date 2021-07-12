31a0a1cc5b9d3f323ea870532569117f273188c2
Michel Megens <michel@michelmegens.net>
2021-07-13 00:29:20 +0200

    tests: routing: implement test cases
    Implement missing test cases for the composite router unit tests.
    
    Signed-off-by: Michel Megens <michel@michelmegens.net>

--- SensateIoT.Platform.Network.Tests/Routing/CompositeRouterTests.cs
+++ SensateIoT.Platform.Network.Tests/Routing/CompositeRouterTests.cs
@@ -17,6 +17,7 @@ using SensateIoT.Platform.Network.Common.Caching.Abstract;
 using SensateIoT.Platform.Network.Common.Collections.Abstract;
 using SensateIoT.Platform.Network.Common.Exceptions;
 using SensateIoT.Platform.Network.Common.Routing;
+using SensateIoT.Platform.Network.Common.Routing.Abstract;
 using SensateIoT.Platform.Network.Contracts.DTO;
 using SensateIoT.Platform.Network.Data.Abstract;
 using SensateIoT.Platform.Network.Data.DTO;
@@ -33,7 +34,7 @@ namespace SensateIoT.Platform.Network.Tests.Routing
 		[TestMethod]
 		public void CanExecuteRouters()
 		{
-			var router = CreateCompositeRouter();
+			var router = CreateCompositeRouter(Sensor, Account, ApiKey);
 
 			var r1 = new RouterStub();
 			var r2 = new RouterStub();
@@ -56,39 +57,72 @@ namespace SensateIoT.Platform.Network.Tests.Routing
 		[TestMethod]
 		public void CannotRouteFromBannedAccount()
 		{
+			var account = new Account {ID = Guid.NewGuid(), HasBillingLockout = false, IsBanned = true};
+			var sensor = new Sensor {ID = ObjectId.GenerateNewId(), AccountID = account.ID};
+			var apikey = new ApiKey {AccountID = account.ID};
+			var router = CreateCompositeRouter(sensor, account, apikey);
+
+			this.TryExecuteRouter(router, sensor);
 		}
 
 		[TestMethod]
 		public void CannotRouteBillingLockedAccount()
 		{
+			var account = new Account {ID = Guid.NewGuid(), HasBillingLockout = true};
+			var sensor = new Sensor {ID = ObjectId.GenerateNewId(), AccountID = account.ID};
+			var apikey = new ApiKey {AccountID = account.ID};
+			var router = CreateCompositeRouter(sensor, account, apikey);
+
+			this.TryExecuteRouter(router, sensor);
 		}
 
 		[TestMethod]
 		public void CannotRouteWithReadOnlyKey()
 		{
+			var account = new Account {ID = Guid.NewGuid()};
+			var sensor = new Sensor {ID = ObjectId.GenerateNewId(), AccountID = account.ID};
+			var apikey = new ApiKey {AccountID = account.ID, IsReadOnly = true};
+			var router = CreateCompositeRouter(sensor, account, apikey);
 
+			this.TryExecuteRouter(router, sensor);
 		}
 
 		[TestMethod]
 		public void CannotRouteWithRevokedKey()
 		{
+			var account = new Account {ID = Guid.NewGuid()};
+			var sensor = new Sensor {ID = ObjectId.GenerateNewId(), AccountID = account.ID};
+			var apikey = new ApiKey {AccountID = account.ID, IsRevoked = true};
+			var router = CreateCompositeRouter(sensor, account, apikey);
 
+			this.TryExecuteRouter(router, sensor);
 		}
 
 		[TestMethod]
 		public void CannotRouteWithoutAccount()
 		{
+			var account = new Account {ID = Guid.NewGuid(), HasBillingLockout = true};
+			var sensor = new Sensor {ID = ObjectId.GenerateNewId(), AccountID = Guid.NewGuid()};
+			var apikey = new ApiKey {AccountID = account.ID};
+			var router = CreateCompositeRouter(sensor, account, apikey);
+
+			this.TryExecuteRouter(router, sensor);
 		}
 
 		[TestMethod]
 		public void CannotRouteWithoutSensorKey()
 		{
+			var account = new Account {ID = Guid.NewGuid(), HasBillingLockout = true};
+			var sensor = new Sensor {ID = ObjectId.GenerateNewId(), AccountID = account.ID};
+			var router = CreateCompositeRouter(sensor, account, null);
+
+			this.TryExecuteRouter(router, sensor);
 		}
 
 		[TestMethod]
 		public void SensorCannotBeNull()
 		{
-			var router = CreateCompositeRouter();
+			var router = CreateCompositeRouter(Sensor, Account, ApiKey);
 
 			var r1 = new RouterStub();
 			var r2 = new RouterStub();
@@ -109,10 +143,31 @@ namespace SensateIoT.Platform.Network.Tests.Routing
 			Assert.IsFalse(r3.Executed);
 		}
 
+		private void TryExecuteRouter(IMessageRouter router, Sensor sensor)
+		{
+			var r1 = new RouterStub();
+			var r2 = new RouterStub();
+			var r3 = new RouterStub();
+
+			router.AddRouter(r1);
+			router.AddRouter(r2);
+			router.AddRouter(r3);
+
+			var msg = new Message {
+				SensorId = sensor.ID
+			};
+
+			router.Route(AsList(msg));
+
+			Assert.IsFalse(r1.Executed);
+			Assert.IsFalse(r2.Executed);
+			Assert.IsFalse(r3.Executed);
+		}
+
 		[TestMethod]
 		public void CanCancelRoutesGracefully()
 		{
-			var router = CreateCompositeRouter();
+			var router = CreateCompositeRouter(Sensor, Account, ApiKey);
 
 			var r1 = new RouterStub();
 			var r2 = new RouterStub { Cancel = true };
@@ -135,7 +190,7 @@ namespace SensateIoT.Platform.Network.Tests.Routing
 		[TestMethod]
 		public void CanCatchRouterExceptions()
 		{
-			var router = CreateCompositeRouter();
+			var router = CreateCompositeRouter(Sensor, Account, ApiKey);
 
 			var r1 = new RouterStub();
 			var r2 = new RouterStub { Exception = new RouterException("TestRouter", "testing exception catching.") };
@@ -158,7 +213,7 @@ namespace SensateIoT.Platform.Network.Tests.Routing
 		[TestMethod]
 		public void CannotCatchOtherExceptions()
 		{
-			var router = CreateCompositeRouter();
+			var router = CreateCompositeRouter(Sensor, Account, ApiKey);
 
 			var r1 = new RouterStub();
 			var r2 = new RouterStub { Exception = new InvalidOperationException() };
@@ -183,7 +238,7 @@ namespace SensateIoT.Platform.Network.Tests.Routing
 			var queue = new Mock<IRemoteNetworkEventQueue>();
 
 			queue.Setup(x => x.EnqueueEvent(It.IsAny<NetworkEvent>())).Callback(() => count += 1);
-			var router = new CompositeRouter(CreateRoutingCache(), queue.Object, logger.Object);
+			var router = new CompositeRouter(CreateRoutingCache(Sensor, Account, ApiKey), queue.Object, logger.Object);
 
 			var r1 = new RouterStub();
 			var r2 = new RouterStub();
@@ -199,22 +254,22 @@ namespace SensateIoT.Platform.Network.Tests.Routing
 			Assert.AreEqual(1, count);
 		}
 
-		private static IRoutingCache CreateRoutingCache()
+		private static IRoutingCache CreateRoutingCache(Sensor sensor, Account account, ApiKey key)
 		{
 			var cache = new Mock<IRoutingCache>();
 
-			cache.Setup(x => x[Sensor.ID]).Returns(Sensor);
-			cache.Setup(x => x.GetAccount(Account.ID)).Returns(Account);
-			cache.Setup(x => x.GetApiKey(Sensor.SensorKey)).Returns(ApiKey);
+			cache.Setup(x => x[sensor.ID]).Returns(sensor);
+			cache.Setup(x => x.GetAccount(account.ID)).Returns(Account);
+			cache.Setup(x => x.GetApiKey(sensor.SensorKey)).Returns(key);
 			return cache.Object;
 		}
 
-		private static CompositeRouter CreateCompositeRouter()
+		private static CompositeRouter CreateCompositeRouter(Sensor sensor, Account account, ApiKey key)
 		{
 			var logger = new Mock<ILogger<CompositeRouter>>();
 			var queue = new Mock<IRemoteNetworkEventQueue>();
 
-			return new CompositeRouter(CreateRoutingCache(), queue.Object, logger.Object);
+			return new CompositeRouter(CreateRoutingCache(sensor, account, key), queue.Object, logger.Object);
 		}
 
 		private static IList<IPlatformMessage> AsList(IPlatformMessage message)
