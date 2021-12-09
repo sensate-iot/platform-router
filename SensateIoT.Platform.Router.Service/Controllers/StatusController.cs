9b75031c144a99e23b352607eb40143a4977875a
Michel Megens <michel@michelmegens.net>
2021-12-09 10:21:27 +0100

    health-check: controller: decorate the status controller
    Decorate the status controller with the correct attributes and add the
    `Controller` class as a base.
    
    Signed-off-by: Michel Megens <michel@michelmegens.net>

---
+++ SensateIoT.Platform.Router.Service/Controllers/StatusController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SensateIoT.Platform.Router.Service.Controllers
+{
+	[Produces("application/json")]
+	[Route("router/v1/[controller]")]
+	public class StatusController : Controller
+	{
+		private readonly ILogger<StatusController> m_logger;
+
+		public StatusController(ILogger<StatusController> logger)
+		{
+			this.m_logger = logger;
+		}
+	}
+}
